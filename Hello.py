7a0ff1c785d1190916449cb54c8f9feee81edf01
Christian <elchristog@gmail.com>
2023-10-19 10:42:50 +0000

    commented everithing

--- Hello.py
+++ Hello.py
@@ -5,30 +5,31 @@ LOGGER = get_logger(__name__)
 
 
 def run():
+    st.write("# Welcome to Streamlit! 👋")
     # st.set_page_config(page_title="Company data driven", page_icon="💺", layout="centered", initial_sidebar_state="expanded")
 
-    with st.sidebar:
-      # st.image("cell-tower.png", width=100, use_column_width=False)
-      menu = st.sidebar.radio("Menu", ["Home", "Proyectos NO rentables", "Proyectos rentables", "Oportunidades"])
-      st.write("---") 
+    # with st.sidebar:
+    #   # st.image("cell-tower.png", width=100, use_column_width=False)
+    #   menu = st.sidebar.radio("Menu", ["Home", "Proyectos NO rentables", "Proyectos rentables", "Oportunidades"])
+    #   st.write("---") 
 
-    # Display the selected page
-    if menu == "Home":
+    # # Display the selected page
+    # if menu == "Home":
 
-      st.write("# Welcome to Streamlit! 👋")
+    #   st.write("# Welcome to Streamlit! 👋")
 
-      # st.sidebar.success("Select a demo above.")
+    #   # st.sidebar.success("Select a demo above.")
 
-      st.markdown(
-          """
-          Streamlit is an open-source app framework built specifically for
-          Machine Learning and Data Science projects.
-          **👈 Select a demo from the sidebar** to see some examples
-          of what Streamlit can do!
-          ### Want to learn more?
-          - Check out [streamlit.io](https://streamlit.io)
-      """
-      )
+    #   st.markdown(
+    #       """
+    #       Streamlit is an open-source app framework built specifically for
+    #       Machine Learning and Data Science projects.
+    #       **👈 Select a demo from the sidebar** to see some examples
+    #       of what Streamlit can do!
+    #       ### Want to learn more?
+    #       - Check out [streamlit.io](https://streamlit.io)
+    #   """
+    #   )
 
 
 if __name__ == "__main__":
