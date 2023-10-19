4d1c1f02887c038186e0cd2c9dbe170c63b705cf
Christian <elchristog@gmail.com>
2023-10-19 10:31:42 +0000

    shorter markdown in home

--- Hello.py
+++ Hello.py
@@ -27,13 +27,6 @@ def run():
           of what Streamlit can do!
           ### Want to learn more?
           - Check out [streamlit.io](https://streamlit.io)
-          - Jump into our [documentation](https://docs.streamlit.io)
-          - Ask a question in our [community
-            forums](https://discuss.streamlit.io)
-          ### See more complex demos
-          - Use a neural net to [analyze the Udacity Self-driving Car Image
-            Dataset](https://github.com/streamlit/demo-self-driving)
-          - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
       """
       )
 
