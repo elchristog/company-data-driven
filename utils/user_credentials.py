4fcd4518a18842a1316bdbc4d34fa69e91cd2808
Christian <elchristog@gmail.com>
2023-10-19 14:12:36 +0000

    user_credentials imports

--- utils/user_credentials.py
+++ utils/user_credentials.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
 def user_credentials(name, authentication_status, username):
     st.write(name) 
