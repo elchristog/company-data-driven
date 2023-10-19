058982131907d14d1c6b54b6f72631ac3662e959
Christian <elchristog@gmail.com>
2023-10-19 12:35:23 +0000

    login.login()

--- Hello.py
+++ Hello.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-import utils.login
+import utils.login as login
 
 from streamlit.logger import get_logger
 
@@ -8,6 +8,7 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.write("# Please log in")
+    login.login()
     # st.set_page_config(page_title="Company data driven", page_icon="💺", layout="centered", initial_sidebar_state="expanded")
 
     # with st.sidebar:
