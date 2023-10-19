b15d93def4ea4756c61fd1990f4680c341f4f285
Christian <elchristog@gmail.com>
2023-10-19 12:32:56 +0000

    def login() and config.yaml

--- utils/login.py
+++ utils/login.py
@@ -5,22 +5,22 @@ import yaml
 from yaml.loader import SafeLoader
 
 
-
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-    authenticator = stauth.Authenticate(
-        config['credentials'],
-        config['cookie']['name'],
-        config['cookie']['key'],
-        config['cookie']['expiry_days'],
-        config['preauthorized']
-    )
-    name, authentication_status, username = authenticator.login('Login', 'main')
-   
-    if st.session_state["authentication_status"]:
-        # st.write(f'# {st.secrets["PROJECT_NAME"]} {st.secrets["PROJECT_ICON"]}')
-        st.markdown(""" ### Your tasks """)
-    elif st.session_state["authentication_status"] is False:
-        st.error('Username/password is incorrect')
-    elif st.session_state["authentication_status"] is None:
-        st.warning('Please enter your username and password')
+def login():
+    with open('config.yaml') as file:
+        config = yaml.load(file, Loader=SafeLoader)
+        authenticator = stauth.Authenticate(
+            config['credentials'],
+            config['cookie']['name'],
+            config['cookie']['key'],
+            config['cookie']['expiry_days'],
+            config['preauthorized']
+        )
+        name, authentication_status, username = authenticator.login('Login', 'main')
+    
+        if st.session_state["authentication_status"]:
+            # st.write(f'# {st.secrets["PROJECT_NAME"]} {st.secrets["PROJECT_ICON"]}')
+            st.markdown(""" ### Your tasks """)
+        elif st.session_state["authentication_status"] is False:
+            st.error('Username/password is incorrect')
+        elif st.session_state["authentication_status"] is None:
+            st.warning('Please enter your username and password')
