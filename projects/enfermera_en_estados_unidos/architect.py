22609d0d89c0d9e72c5201099f41713b75af8fd8
Christian <elchristog@gmail.com>
2023-10-20 13:36:50 +0000

    testing architect

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -3,7 +3,31 @@ import streamlit as st
 def architect(user_id, role_id):
     # admin
     if role_id == 1:
-        pass
+            
+        st.set_page_config(page_title="Company data driven", page_icon="💺", layout="centered", initial_sidebar_state="expanded")
+
+        with st.sidebar:
+            # st.image("cell-tower.png", width=100, use_column_width=False)
+            menu = st.sidebar.radio("Menu", ["Home", "Proyectos NO rentables", "Proyectos rentables", "Oportunidades"])
+            st.write("---") 
+
+        # Display the selected page
+        if menu == "Home":
+
+            st.write("# Welcome to Streamlit! 👋")
+
+            # st.sidebar.success("Select a demo above.")
+
+            st.markdown(
+                """
+                Streamlit is an open-source app framework built specifically for
+                Machine Learning and Data Science projects.
+                **👈 Select a demo from the sidebar** to see some examples
+                of what Streamlit can do!
+                ### Want to learn more?
+                - Check out [streamlit.io](https://streamlit.io)
+            """
+            )   
 
     # customer
     if role_id == 6:
