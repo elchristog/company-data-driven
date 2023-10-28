7b2487a800161b96d4b800bd383a24d88cf8980e
Christian <elchristog@gmail.com>
2023-10-28 16:20:59 +0000

    gcloud_bigquery_client in user credentials

--- utils/user_credentials.py
+++ utils/user_credentials.py
@@ -5,13 +5,16 @@ import utils.project_handler as ph
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
-
-def user_credentials(name, authentication_status, username):
+def gcloud_bigquery_client():
     credentials = service_account.Credentials.from_service_account_info(
             st.secrets["gcp_service_account"]
         )
     client = bigquery.Client(credentials=credentials)
+    return client
 
+
+def user_credentials(name, authentication_status, username):
+    client = gcloud_bigquery_client()
     # @st.cache_data(ttl=600) # Uses st.cache_data to only rerun when the query changes or after 10 min.
     def run_query(query):
         query_job = client.query(query)
