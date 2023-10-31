3702ca21d2dea40824130e4fcd3e3d5daf0e61f1
Christian <elchristog@gmail.com>
2023-10-31 14:33:40 +0000

    run_query_1_day

--- utils/user_credentials.py
+++ utils/user_credentials.py
@@ -15,8 +15,14 @@ def gcloud_bigquery_client():
 
 client = gcloud_bigquery_client()
 
-@st.cache_data(ttl=3)
 def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    rows = [dict(row) for row in rows_raw]
+    return rows
+
+@st.cache_data(ttl=86400)
+def run_query_1_day(query):
     query_job = client.query(query)
     rows_raw = query_job.result()
     rows = [dict(row) for row in rows_raw]
@@ -24,7 +30,7 @@ def run_query(query):
 
 def user_credentials(name, authentication_status, username):
     # @st.cache_data(ttl=600) # Uses st.cache_data to only rerun when the query changes or after 10 min.
-    rows = run_query(f"SELECT u.id AS user_id, u.username, u.status, u.project_id, r.id AS role_id, r.name AS role_name, p.icon, p.logo_url, p.name, p.title   FROM `company-data-driven.global.users` AS u INNER JOIN `company-data-driven.global.role_assignment` AS ra ON u.id = ra.user_id INNER JOIN `company-data-driven.global.roles` AS r ON ra.role_id = r.id INNER JOIN `company-data-driven.global.projects` AS p ON u.project_id = p.id WHERE username = '{username}';")
+    rows = run_query_1_day(f"SELECT u.id AS user_id, u.username, u.status, u.project_id, r.id AS role_id, r.name AS role_name, p.icon, p.logo_url, p.name, p.title   FROM `company-data-driven.global.users` AS u INNER JOIN `company-data-driven.global.role_assignment` AS ra ON u.id = ra.user_id INNER JOIN `company-data-driven.global.roles` AS r ON ra.role_id = r.id INNER JOIN `company-data-driven.global.projects` AS p ON u.project_id = p.id WHERE username = '{username}';")
 
     if len(rows) == 1:
         user_id = rows[0].get('user_id')
