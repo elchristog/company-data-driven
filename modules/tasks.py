cc981429af811b3359c20a9067b3e3fa9988840f
Christian <elchristog@gmail.com>
2023-10-31 12:32:55 +0000

    _client

--- modules/tasks.py
+++ modules/tasks.py
@@ -7,7 +7,7 @@ import utils.user_credentials as uc
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 @st.cache
-def tasks_visualizer(user_id, project_name, client, divider):
+def tasks_visualizer(user_id, project_name, _client, divider):
     rows = uc.run_query(f"SELECT id, creation_date, description, commit_finish_date, status  FROM `company-data-driven.{project_name}.tasks` WHERE responsible_user_id = {user_id} AND status IN ('to_start', 'on_execution', 'delayed');", client) #finished, canceled, unfulfilled
     if len(rows) == 0:
         st.success('You have no pending tasks, very good!', icon="😎")
