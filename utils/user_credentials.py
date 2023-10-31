74e90ac90968a777a0c00524fae16ed15daa96c8
Christian <elchristog@gmail.com>
2023-10-31 12:19:50 +0000

    deleted cache again

--- utils/user_credentials.py
+++ utils/user_credentials.py
@@ -12,7 +12,6 @@ def gcloud_bigquery_client():
     client = bigquery.Client(credentials=credentials)
     return client
 
-@st.cache_data(ttl=600)
 def run_query(query, client):
     query_job = client.query(query)
     rows_raw = query_job.result()
