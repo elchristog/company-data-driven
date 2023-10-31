e5e83f939a75a617f81775cbed93e7bfb45fdb66
Christian <elchristog@gmail.com>
2023-10-31 12:55:56 +0000

    no client in project handler

--- utils/project_handler.py
+++ utils/project_handler.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import projects.enfermera_en_estados_unidos.architect as eeeu_architect
 
-def project_handler(user_id, project_id, role_id, role_name, project_name, project_title, project_icon, project_logo_url, client):
+def project_handler(user_id, project_id, role_id, role_name, project_name, project_title, project_icon, project_logo_url):
     # enfermera_en_estados_unidos
     if project_id == 1:
-        eeeu_architect.architect(user_id, role_id, project_id, project_name, project_title, project_icon, project_logo_url, client)
+        eeeu_architect.architect(user_id, role_id, project_id, project_name, project_title, project_icon, project_logo_url)
