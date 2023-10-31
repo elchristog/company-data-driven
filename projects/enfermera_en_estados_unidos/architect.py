ab26dcf6b9dcab410756cb3e33109c6774574515
Christian <elchristog@gmail.com>
2023-10-31 15:06:11 +0000

    no expander

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -17,9 +17,8 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
             with tab1:
                 tap.title_and_paragraph(project_title + project_icon, "Seguimiento y mantenimiento de los usuarios", "h1", 0)
                 tap.title_and_paragraph("Tus tareas", "Gestiona tus tareas (delayed tasks will be labeled as unfulfilled after 5 days of the commitment date)", "h2", 0)
-                with st.expander("Administrar tareas"):
-                    tasks = t.tasks_visualizer(user_id, project_name, 0)
-                    t.tips_tasks_ia(tasks, 0)
+                tasks = t.tasks_visualizer(user_id, project_name, 0)
+                t.tips_tasks_ia(tasks, 0)
                 tap.title_and_paragraph("Tus logros", "Visualiza tu crecimiento", "h3", 0)
                 t.tasks_achievements(user_id, project_name, tasks, 1)
             with tab2:
