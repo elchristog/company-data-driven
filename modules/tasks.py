d9421556eb123266856912ff9496a15e74e685aa
Christian <elchristog@gmail.com>
2023-10-31 11:33:26 +0000

    @st.cache_data befire task visualizer

--- modules/tasks.py
+++ modules/tasks.py
@@ -7,6 +7,7 @@ import utils.user_credentials as uc
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 def tasks_visualizer(user_id, project_name, client, divider):
+    @st.cache_data
     rows = uc.run_query(f"SELECT id, creation_date, description, commit_finish_date, status  FROM `company-data-driven.{project_name}.tasks` WHERE responsible_user_id = {user_id} AND status IN ('to_start', 'on_execution', 'delayed');", client) #finished, canceled, unfulfilled
     if len(rows) == 0:
         st.success('You have no pending tasks, very good!', icon="😎")
