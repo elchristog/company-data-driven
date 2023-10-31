3ad7fc9ca8785980813234c3cb6d487175020a95
Christian <elchristog@gmail.com>
2023-10-31 14:43:36 +0000

    run_query_insert_update

--- modules/tasks.py
+++ modules/tasks.py
@@ -46,9 +46,9 @@ def tasks_visualizer(user_id, project_name, divider):
             update_task_status_button = st.button("Update status")
             def update_task_status(task_id, new_status, today_str):
                 if new_status == 'on_execution':
-                    uc.run_query_instant(f"UPDATE `company-data-driven.{project_name}.tasks` SET status = '{new_status}', on_execution_date = '{today_str}' WHERE id = {task_id}")
+                    uc.run_query_insert_update(f"UPDATE `company-data-driven.{project_name}.tasks` SET status = '{new_status}', on_execution_date = '{today_str}' WHERE id = {task_id}")
                 if new_status == 'finished':
-                    uc.run_query_instant(f"UPDATE `company-data-driven.{project_name}.tasks` SET status = '{new_status}', finished_date = '{today_str}' WHERE id = {task_id}")
+                    uc.run_query_insert_update(f"UPDATE `company-data-driven.{project_name}.tasks` SET status = '{new_status}', finished_date = '{today_str}' WHERE id = {task_id}")
             if update_task_status_button:
                 today = datetime.date.today()
                 today_str = today.strftime("%Y-%m-%d")
