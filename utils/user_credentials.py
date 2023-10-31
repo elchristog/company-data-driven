35799e020e83c133c3e170e64aff501fbef5f7ee
Christian <elchristog@gmail.com>
2023-10-31 12:57:02 +0000

    no client in project handler

--- utils/user_credentials.py
+++ utils/user_credentials.py
@@ -79,7 +79,7 @@ def user_credentials(name, authentication_status, username):
             else:
                 st.error('User has multiple projects')
              
-        ph.project_handler(user_id, project_id, role_id, role_name, project_name, project_title, project_icon, project_logo_url, client)
+        ph.project_handler(user_id, project_id, role_id, role_name, project_name, project_title, project_icon, project_logo_url)
 
 
 
