bd96de8fe822a71f6b12b850f13b1e951203e68a
Christian <elchristog@gmail.com>
2023-11-01 20:22:01 +0000

    Role confirmation

--- utils/users_handling.py
+++ utils/users_handling.py
@@ -39,8 +39,6 @@ def user_creation(user_id, project_id, project_name):
         options = project_names,
         index = None
     )
-    if selected_project_confirmation is not None:
-        selected_project_id_confirmation = project_ids[project_names.index(selected_project_confirmation)]
     if selected_project is not None and selected_project_confirmation is not None:
         if selected_project == selected_project_confirmation:
             st.success('Project confirmed', icon = '🎈')
@@ -59,6 +57,17 @@ def user_creation(user_id, project_id, project_name):
         options = roles_names,
         index = None
     )
+    selected_role_id = roles_ids[roles_names.index(user_role)]
+    user_role_confirmation = st.selectbox(
+        label = "Confirm user role",
+        options = roles_names,
+        index = None
+    )
+    if user_role is not None and user_role_confirmation is not None:
+        if user_role == user_role_confirmation:
+            st.success('Role confirmed', icon = '🎈')
+        else:
+            st.error('Incorrect role', icon = '🀄')
 
     user_first_name = st.text_input("Write the user first name:")
     user_last_name = st.text_input("Write the user last name:")
