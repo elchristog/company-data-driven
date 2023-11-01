19dbed8ac858cc330a0d22b268daa5c5aba526cc
Christian <elchristog@gmail.com>
2023-11-01 19:57:59 +0000

    pailongas, username not filter

--- utils/users_handling.py
+++ utils/users_handling.py
@@ -15,22 +15,22 @@ def user_creation(user_id, project_id, project_name):
     username = st.text_input("Write the username:")
     check_username_availability = st.button("Check Availability")
     if check_username_availability: #name, birthdate, country, gender, user_creator, email, project_id
-        checking_username_query = uc.run_query_30_m(f"SELECT id FROM `company-data-driven.global.users` WHERE username = '{username}';")
+        checking_username_query = uc.run_query_instant(f"SELECT id FROM `company-data-driven.global.users` WHERE username = '{username}';")
         if len(checking_username_query) > 0:
             st.error('Username is not available', icon = '👻')
         else:
             st.success('Username available', icon = '🪬')
-            max_id_users = uc.run_query_instant(f"SELECT 1 + MAX(id) AS max_id FROM `company-data-driven.global.users`;")[0].get('max_id')
-            get_projects = uc.run_query_instant(f"SELECT id, name FROM `company-data-driven.global.projects`;")
-            project_ids = []
-            project_names = []
-            for row in get_projects:
-                project_ids.append(row.get('id'))
-                project_names.append(row.get('name'))
-            selected_project = st.selectbox(
-                label = "Select the project for the user",
-                options = project_names,
-                index = None
-            )
+    max_id_users = uc.run_query_instant(f"SELECT 1 + MAX(id) AS max_id FROM `company-data-driven.global.users`;")[0].get('max_id')
+    get_projects = uc.run_query_instant(f"SELECT id, name FROM `company-data-driven.global.projects`;")
+    project_ids = []
+    project_names = []
+    for row in get_projects:
+        project_ids.append(row.get('id'))
+        project_names.append(row.get('name'))
+    selected_project = st.selectbox(
+        label = "Select the project for the user",
+        options = project_names,
+        index = None
+    )
 
 
