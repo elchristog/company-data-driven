7bcc6d40a600f70b4e935693fb82ddd8f15b23f4
Christian <elchristog@gmail.com>
2023-11-01 16:42:33 +0000

    st.button("Start Hashing")

--- utils/users_handling.py
+++ utils/users_handling.py
@@ -5,4 +5,6 @@ import streamlit_authenticator as stauth
 def hashing():
     password_to_hash = st.text_input("Write the password to hash:")
     hashed_passwords = stauth.Hasher([password_to_hash]).generate()
-    st.text(hashed_passwords)
+    hashing_button = st.button("Start Hashing")
+    if hashing_button:
+        st.text(hashed_passwords)
