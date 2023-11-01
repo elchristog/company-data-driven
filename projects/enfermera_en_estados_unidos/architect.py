1300021c72fac39ba3a7ea79b802818d92ba146b
Christian <elchristog@gmail.com>
2023-11-01 16:03:35 +0000

    sub sidebar in home

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -11,11 +11,11 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
     if role_id != 6:
         with st.sidebar:
             st.image(project_logo_url, width=50, use_column_width=False)
-            st.sidebar.radio('drops sub-menu', options=['add drops', 'view drops'])
             menu = st.sidebar.radio(project_title, ["Home", "Trafico y SEO", "Whatsapp", "Trip Wire"])
             st.write("---") 
 
         if menu == "Home":
+            st.sidebar.radio('drops sub-menu', options=['add drops', 'view drops'])
             tab1, tab2, tab3 = st.tabs(["Tareas", "Asignar", "Eliminar"])
             with tab1:
                 # tap.title_and_paragraph(project_title + project_icon, "Seguimiento y mantenimiento de los usuarios", "h1", 0)
