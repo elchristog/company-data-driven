56b4947164a7c8fb468dbeeb5c4f66c13834bdeb
Christian <elchristog@gmail.com>
2023-11-02 20:47:47 +0000

    ;

--- modules/tester.py
+++ modules/tester.py
@@ -4,5 +4,5 @@ import utils.user_credentials as uc
 
 
 def tester(): # project_name, questions_table_name, num_questions, user_id
-    questions = uc.run_query_1_h(f"SELECT * FROM `company-data-driven.enfermera_en_estados_unidos.nclex_questions` ORDER BY RAND() LIMIT 3")
+    questions = uc.run_query_1_h(f"SELECT * FROM `company-data-driven.enfermera_en_estados_unidos.nclex_questions` ORDER BY RAND() LIMIT 3;")
     st.write(questions)
