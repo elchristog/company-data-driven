bd854e025825b2421f680ed3e7ef4794b2778c77
Christian <elchristog@gmail.com>
2023-11-02 20:46:35 +0000

    st.write(questions query

--- modules/tester.py
+++ modules/tester.py
@@ -3,5 +3,6 @@ import streamlit as st
 import utils.user_credentials as uc
 
 
-def tester():
-    st.success("tester")
+def tester(): # project_name, questions_table_name, num_questions, user_id
+    questions = uc.run_query_1_h(f"SELECT * FROM `company-data-driven.enfermera_en_estados_unidos.nclex_questions` ORDER BY RAND() LIMIT 3")
+    st.write(questions)
