1a92a6f4dca494df7fb9e110d566274d3abf2cec
Christian <elchristog@gmail.com>
2023-11-02 21:06:28 +0000

    item idea

--- modules/tester.py
+++ modules/tester.py
@@ -6,3 +6,5 @@ import utils.user_credentials as uc
 def tester(project_name, questions_table_name, num_questions, user_id): 
     questions = uc.run_query_1_h(f"SELECT * FROM `company-data-driven.{project_name}.{questions_table_name}` ORDER BY RAND() LIMIT {num_questions};")
     st.write(questions)
+
+    st.write("🔹 **A)** " + "pepe")
