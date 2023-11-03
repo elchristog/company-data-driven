32f9d1d3b14c6ca22c98f1a1ee2bad6772309fe4
Christian <elchristog@gmail.com>
2023-11-03 14:26:51 +0000

    steps in funnel

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -14,7 +14,7 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
     if role_id != 6:
         with st.sidebar:
             st.image(project_logo_url, width=50, use_column_width=False)
-            menu_options = ["Home", "Trafico y SEO", "Whatsapp", "Trip Wire"]
+            menu_options = ["Home", "Trafico y SEO", "Click bitly", "Whatsapp", "Trip Wire", "Remarketing", "Web tool", "Contract", "Contract Remarketing", "Step1", "Step2"]
             if role_id == 1:
                 menu_options.extend(['Users Admin'])
             menu = st.sidebar.radio(project_title, menu_options)
