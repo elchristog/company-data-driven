3f8b6caa7922f8f3f41717aad82e87ab1d72aefd
Christian <elchristog@gmail.com>
2023-11-04 19:52:58 +0000

    test_achievements use score as y in plot

--- modules/tester.py
+++ modules/tester.py
@@ -394,8 +394,8 @@ def test_achievements(project_name, user_id, attempts_table_name):
     st.header("Your evolution")
     user_score_evolution = uc.run_query_1_day(f"SELECT ta.attempt_date, ta.success_rate AS score FROM `company-data-driven.{project_name}.{attempts_table_name}` AS ta WHERE ta.user_id = {user_id} ORDER BY ta.id ASC;")
     user_score_evolution.sort(key=lambda x: x["attempt_date"])
-    user_score_evolution_df = pd.DataFrame(user_score_evolution, columns = ["attempt_date","success_rate"])
+    user_score_evolution_df = pd.DataFrame(user_score_evolution, columns = ["attempt_date","score"])
     st.table(user_score_evolution_df)
-    st.bar_chart(user_score_evolution_df, x="attempt_date", y="success_rate")
+    st.bar_chart(user_score_evolution_df, x="attempt_date", y="score")
 
     
