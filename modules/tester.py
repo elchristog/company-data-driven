2f70024f7f779a7d2e2b9a282f15cbfbd19969f0
Christian <elchristog@gmail.com>
2023-11-04 19:27:29 +0000

    test_achievements removed dictionary writing

--- modules/tester.py
+++ modules/tester.py
@@ -372,7 +372,6 @@ def test_achievements(project_name, user_id, attempts_table_name):
             if len(user_score_and_position) < 1 or user_score_and_position is None < 0:
                 st.warning(f"You have not presented your test", icon = "🫥")
             else:
-                st.write(ranking[user_score_and_position[0]])
                 percentile = 100 * (len(ranking)-ranking[user_score_and_position[0]].get("position"))/len(ranking)
                 if percentile > 80:
                     st.success(f"You got **{ranking[user_score_and_position[0]].get('score')}%** of today's questions right", icon = "😸")
