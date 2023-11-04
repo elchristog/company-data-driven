057d7e50420a3ee4f92a03f2026be4fef5f3bdac
Christian <elchristog@gmail.com>
2023-11-04 20:53:44 +0000

    Total evolution

--- modules/tester.py
+++ modules/tester.py
@@ -409,6 +409,7 @@ def test_achievements(project_name, user_id, attempts_table_name):
 
         # metrics
         col1, col2, col3 = st.columns(3)
+        col1.header("Total evolution")
         col1.metric(label="# Total Tests", value = len(user_score_evolution), delta= 0)
         col1.metric(label="# Avg score", value = user_score_evolution_df.score.mean(), delta= 0)
 
