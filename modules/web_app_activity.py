1dddb8accde60d52a47a4cdef5f7b6d6d747fdea
Christian <elchristog@gmail.com>
2023-11-05 15:34:29 +0000

    login_activity top_5_users_without_login

--- modules/web_app_activity.py
+++ modules/web_app_activity.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import datetime
 
+import utils.user_credentials as uc
+
 def login_activity():
-    st.success("My douglas")
+    top_5_users_without_login = uc.run_query_6_h(f"SELECT u.id, u.username, u.name, u.lastname, u.last_login_date, DATE_DIFF(CURRENT_DATE(), u.last_login_date, DAY) AS days_without_login FROM `company-data-driven.global.users` AS u INNER JOIN `company-data-driven.global.role_assignment` AS ra ON u.id = ra.user_id WHERE ra.role_id = 6 AND u.status = 'active' ORDER BY days_without_login DESC LIMIT 5")
+    st.table(top_5_users_without_login)
