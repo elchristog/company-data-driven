64bacaa28c166ce119b5065cbda4aa1a3e7408b6
Christian <elchristog@gmail.com>
2023-11-06 17:28:26 +0000

    def one_resource():  testing link

--- modules/resources.py
+++ modules/resources.py
@@ -5,3 +5,6 @@ import utils.user_credentials as uc
 
 def one_resource():
     st.metric(label="# Month Tests", value = '🗂️')
+    link = f"[Discuss answers with my group](url)"
+    st.markdown(link, unsafe_allow_html=True)
+    st.metric(label="# Month Tests", value = st.markdown(link, unsafe_allow_html=True))
