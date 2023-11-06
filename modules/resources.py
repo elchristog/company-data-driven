ccf41076c1abaa29ff9b139b1f258eaef214d7fd
Christian <elchristog@gmail.com>
2023-11-06 17:32:59 +0000

    one_resource card_index_dividers

--- modules/resources.py
+++ modules/resources.py
@@ -6,7 +6,4 @@ import utils.user_credentials as uc
 # https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
 
 def one_resource():
-    st.metric(label="# Month Tests", value = '🗂️')
-    link = f"[Discuss answers with my group](url)"
-    st.markdown(link, unsafe_allow_html=True)
-    st.link_button(":file_folder: Go to gallery", "https://streamlit.io/gallery")
+    st.link_button(":card_index_dividers: Go to gallery", "https://streamlit.io/gallery")
