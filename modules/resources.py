a8142f3ad1d9e51fc8b817fb1fbe90f86cf4339d
Christian <elchristog@gmail.com>
2023-11-06 18:01:38 +0000

    resources links buttons

--- modules/resources.py
+++ modules/resources.py
@@ -8,17 +8,22 @@ import utils.user_credentials as uc
 def resources(project_name, user_id, r_1 = [], r_2 = [], r_3 = [], r_4 = [], r_5 = [], r_6 = [], r_7 = [], r_8 = [], r_9 = [], r_10 = [], r_11 = []):
     # Each resource must be: [':selfie:', 'name', 'link_url'] [icon, button_name, link_url]
 
-    st.write(r_1)
-    st.write(len(r_1))
-
-    st.write(r_2)
-    st.write(len(r_2))
-
-
-    col1, col2, col3, col4 = st.columns(4)
+    col1, col2, col3 = st.columns(3)
     with col1:
         st.link_button(":card_index_dividers: Google Drive", "https://streamlit.io/gallery")
+        st.link_button(f"{r_3[0]} {r_3[1]}", r_3[2]) if len(r_3) > 0 else st.write()
+        st.link_button(f"{r_6[0]} {r_6[1]}", r_6[2]) if len(r_6) > 0 else st.write()
+        st.link_button(f"{r_9[0]} {r_9[1]}", r_9[2]) if len(r_9) > 0 else st.write()
     with col2:
         st.link_button(f"{r_1[0]} {r_1[1]}", r_1[2]) if len(r_1) > 0 else st.write()
+        st.link_button(f"{r_4[0]} {r_4[1]}", r_4[2]) if len(r_4) > 0 else st.write()
+        st.link_button(f"{r_7[0]} {r_7[1]}", r_7[2]) if len(r_7) > 0 else st.write()
+        st.link_button(f"{r_10[0]} {r_10[1]}", r_10[2]) if len(r_10) > 0 else st.write()
     with col3:
         st.link_button(f"{r_2[0]} {r_2[1]}", r_2[2]) if len(r_2) > 0 else st.write()
+        st.link_button(f"{r_5[0]} {r_5[1]}", r_5[2]) if len(r_5) > 0 else st.write()
+        st.link_button(f"{r_8[0]} {r_8[1]}", r_8[2]) if len(r_8) > 0 else st.write()
+        st.link_button(f"{r_11[0]} {r_11[1]}", r_11[2]) if len(r_11) > 0 else st.write()
+
+
+
