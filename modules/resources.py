086a509f344a2c995155418e46c735af179426b6
Christian <elchristog@gmail.com>
2023-11-06 17:56:12 +0000

    resources 3 buttons

--- modules/resources.py
+++ modules/resources.py
@@ -18,3 +18,7 @@ def resources(project_name, user_id, r_1 = [], r_2 = [], r_3 = [], r_4 = [], r_5
     col1, col2, col3, col4 = st.columns(4)
     with col1:
         st.link_button(":card_index_dividers: Google Drive", "https://streamlit.io/gallery")
+    with col2:
+        st.link_button(":card_index_dividers: Google Drive", "https://streamlit.io/gallery") if len(r_1) > 0 else st.write()
+    with col3:
+        st.link_button(":card_index_dividers: Google Drive", "https://streamlit.io/gallery") if len(r_2) > 0 else st.write()
