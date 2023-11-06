b7da5adf18024f51d9bc5c1ac8717b6a61b031e4
Christian <elchristog@gmail.com>
2023-11-06 17:35:15 +0000

    one_resource 4 columns

--- modules/resources.py
+++ modules/resources.py
@@ -6,6 +6,6 @@ import utils.user_credentials as uc
 # https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
 
 def one_resource():
-    col1, col2, col3, col4, col5 = st.columns(5)
+    col1, col2, col3, col4 = st.columns(4)
     with col1:
         st.link_button(":card_index_dividers: Go to gallery", "https://streamlit.io/gallery")
