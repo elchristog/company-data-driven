2cddd8b107c76acdfe8aad02ec18acd26a73cc04
Christian <elchristog@gmail.com>
2023-11-09 14:49:29 +0000

    general_progress Program progress and steps

--- modules/program_progress.py
+++ modules/program_progress.py
@@ -2,5 +2,5 @@ import streamlit as st
 import datetime
 
 def general_progress():
-    st.success('My douglas')
+    st.header('Program progress and steps')
     st.progress(10, text = f"Progreso general: **{10}%**")
