394f45a1624b2abe3dacf56cbaf1210edb7095e4
Christian <elchristog@gmail.com>
2023-11-09 14:43:47 +0000

    general_progress Progreso general: **{10}%**

--- modules/program_progress.py
+++ modules/program_progress.py
@@ -3,4 +3,4 @@ import datetime
 
 def general_progress():
     st.success('My douglas')
-    st.progress(10, text = f"Progreso general: {10}%")
+    st.progress(10, text = f"Progreso general: **{10}%**")
