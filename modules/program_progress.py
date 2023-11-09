fb555ce97b000d9e380bd44ca54a46ba800c921e
Christian <elchristog@gmail.com>
2023-11-09 22:39:36 +0000

    general_progress ps.id in query

--- modules/program_progress.py
+++ modules/program_progress.py
@@ -6,7 +6,7 @@ import utils.user_credentials as uc
 
 def general_progress(user_id, project_name):
     st.header('Program progress and steps')
-    user_progress_table = uc.run_query_3_h(f"SELECT ps.name, ps.description, COALESCE(CAST(upsp_user.creation_date AS STRING),'pending') AS starting_date FROM `company-data-driven.{project_name}.program_steps` AS ps LEFT JOIN (SELECT  * FROM `company-data-driven.{project_name}.user_program_steps_progress` AS upsp WHERE upsp.user_id = {user_id}) AS upsp_user ON ps.id = upsp_user.program_step_id  ORDER BY ps.id;")
+    user_progress_table = uc.run_query_3_h(f"SELECT ps.id, ps.name, ps.description, COALESCE(CAST(upsp_user.creation_date AS STRING),'pending') AS starting_date FROM `company-data-driven.{project_name}.program_steps` AS ps LEFT JOIN (SELECT  * FROM `company-data-driven.{project_name}.user_program_steps_progress` AS upsp WHERE upsp.user_id = {user_id}) AS upsp_user ON ps.id = upsp_user.program_step_id  ORDER BY ps.id;")
     st.progress(5, text = f"Global progress: **{5}%**")
     st.table(user_progress_table)
     st.write(user_progress_table)
