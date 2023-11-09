ade774b9e71c6f35092704405056df100da60dd4
Christian <elchristog@gmail.com>
2023-11-09 16:42:29 +0000

    general_progress comment

--- modules/program_progress.py
+++ modules/program_progress.py
@@ -4,3 +4,5 @@ import datetime
 def general_progress():
     st.header('Program progress and steps')
     st.progress(5, text = f"Global progress: **{5}%**")
+    # steps table, step_name, step_description, status
+    
