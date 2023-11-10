709971dca9c1e76a31441e016e8b36ebc00b0923
Christian <elchristog@gmail.com>
2023-11-10 17:24:41 +0000

    program_steps_guide icon = "🦔")

--- projects/enfermera_en_estados_unidos/modules/program_steps_guide.py
+++ projects/enfermera_en_estados_unidos/modules/program_steps_guide.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 def program_steps_guide(user_actual_step_id):
     if user_actual_step_id == 1:
-        st.success("# Inicio del programa")
+        st.success("### Inicio del programa", icon = "🦔")
