a6e2b0b7e6d6415973f010e47d38e97b770204c5
Christian <elchristog@gmail.com>
2023-11-10 17:23:29 +0000

    program_steps_guide st.success("# Inicio del progr

--- projects/enfermera_en_estados_unidos/modules/program_steps_guide.py
+++ projects/enfermera_en_estados_unidos/modules/program_steps_guide.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
 def program_steps_guide(user_actual_step_id):
-    st.success(user_actual_step_id)
+    if user_actual_step_id == 1:
+        st.success("# Inicio del programa")
