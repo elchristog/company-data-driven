6c785b4c5c4cbcf716bda81ae087251ac242ad0d
Christian <elchristog@gmail.com>
2023-11-14 16:04:11 +0000

    program_steps_guide st.write( fixed //

--- projects/enfermera_en_estados_unidos/modules/program_steps_guide.py
+++ projects/enfermera_en_estados_unidos/modules/program_steps_guide.py
@@ -4,8 +4,4 @@ def program_steps_guide(user_actual_step_id):
     if user_actual_step_id == 1:
         st.success(f"### Inicio del programa")
         with st.expander("See explanation"):
-            st.write(\"\"\"
-                The chart above shows some numbers I picked for you.
-                I rolled actual dice for these, so they're *guaranteed* to
-                be random.
-            \"\"\")
+            st.write("The chart above shows some numbers I picked for you. I rolled actual dice for these, so they're *guaranteed* to be random.")
