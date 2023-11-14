3dd0c48fae6ef91e949a1755585cc207cab042ef
Christian <elchristog@gmail.com>
2023-11-14 21:09:32 +0000

    program_steps_guide Entrevistas de trabajo

--- projects/enfermera_en_estados_unidos/modules/program_steps_guide.py
+++ projects/enfermera_en_estados_unidos/modules/program_steps_guide.py
@@ -112,3 +112,19 @@ def program_steps_guide(user_actual_step_id):
         with st.expander(":seedling: Presentar el examen"):
             st.write(f"F")
 
+
+
+    # if user_actual_step_id == 5:
+        st.success(f"### Entrevistas de trabajo")
+        st.write(f"¡Bienvenido/a al Programa de Enfermería en Estados Unidos! En esta sección, te proporcionaremos actualizaciones continuas con toda la información necesaria para que tengas claridad sobre las tareas y la manera en que deben llevarse a cabo. Esto te permitirá alcanzar con éxito tu objetivo de convertirte en enfermero/a en Estados Unidos.")
+        with st.expander(":seedling: Preparar CV"):
+            st.write(f"F")
+        with st.expander(":seedling: Buscar ofertas"):
+            st.write(f"F")
+        with st.expander(":seedling: Enviar CV"):
+            st.write(f"F")
+        with st.expander(":seedling: Preparar entrevistas"):
+            st.write(f"F")
+        with st.expander(":seedling: Presentar entrevistas"):
+            st.write(f"F")
+
