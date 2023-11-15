810626f1c2bdb3ce6d61a0206ce7a3fa2feebc18
Christian <elchristog@gmail.com>
2023-11-15 15:34:03 +0000

    architect Update customer progress

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -73,7 +73,7 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
                 tap.title_and_paragraph("Create a new question in the NCLEX test " + project_icon, "Remember not add the letters [A), B), C), D)] in the options", "h3", 0)
                 tst.add_question_to_test(project_name, 'nclex_questions', user_id)
             if sub_menu == "Update customer progress":
-                tap.title_and_paragraph("Create a new question in the NCLEX test " + project_icon, "Remember not add the letters [A), B), C), D)] in the options", "h3", 0)
+                tap.title_and_paragraph("Update customer progress " + project_icon, "lala", "h3", 0)
                 pp.update_customer_progress(user_id, project_name, 'program_steps', 'user_program_steps_progress')
 
         if menu == "Users Admin":
