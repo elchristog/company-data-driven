fb269e37db86a2d6bee0b2e1bdfd2d5f57d696e9
Christian <elchristog@gmail.com>
2023-11-15 16:30:01 +0000

    general_progress run_query_instant

--- modules/program_progress.py
+++ modules/program_progress.py
@@ -7,8 +7,8 @@ import utils.user_credentials as uc
 
 def general_progress(user_id, project_name, program_steps_table_tame, program_steps_user_progress_table_name):
     st.header('Program progress and steps')
-    user_progress_table = uc.run_query_3_h(f"SELECT ps.id, ps.name, ps.description, COALESCE(CAST(upsp_user.creation_date AS STRING),'pending') AS starting_date FROM `company-data-driven.{project_name}.{program_steps_table_tame}` AS ps LEFT JOIN (SELECT  * FROM `company-data-driven.{project_name}.{program_steps_user_progress_table_name}` AS upsp WHERE upsp.user_id = {user_id}) AS upsp_user ON ps.id = upsp_user.program_step_id  ORDER BY ps.id;")
-    user_actual_step= uc.run_query_3_h(f"SELECT program_step_id, creation_date, DATE_DIFF(CURRENT_DATE(), creation_date, DAY) AS days_since_achievemen FROM `company-data-driven.{project_name}.{program_steps_user_progress_table_name}` WHERE user_id = {user_id} AND program_step_id = (SELECT MAX(program_step_id) AS actual_step FROM `company-data-driven.{project_name}.{program_steps_user_progress_table_name}` AS upsp WHERE upsp.user_id = {user_id});")
+    user_progress_table = uc.run_query_instant(f"SELECT ps.id, ps.name, ps.description, COALESCE(CAST(upsp_user.creation_date AS STRING),'pending') AS starting_date FROM `company-data-driven.{project_name}.{program_steps_table_tame}` AS ps LEFT JOIN (SELECT  * FROM `company-data-driven.{project_name}.{program_steps_user_progress_table_name}` AS upsp WHERE upsp.user_id = {user_id}) AS upsp_user ON ps.id = upsp_user.program_step_id  ORDER BY ps.id;")
+    user_actual_step= uc.run_query_instant(f"SELECT program_step_id, creation_date, DATE_DIFF(CURRENT_DATE(), creation_date, DAY) AS days_since_achievemen FROM `company-data-driven.{project_name}.{program_steps_user_progress_table_name}` WHERE user_id = {user_id} AND program_step_id = (SELECT MAX(program_step_id) AS actual_step FROM `company-data-driven.{project_name}.{program_steps_user_progress_table_name}` AS upsp WHERE upsp.user_id = {user_id});")
 
     progress = user_actual_step[0].get("program_step_id")/len(user_progress_table)
     if progress >= 0.9 or user_actual_step[0].get("days_since_achievemen") < 5:
