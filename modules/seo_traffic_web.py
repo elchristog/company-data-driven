54188af01e29a68265f3e3f14763424e2d6dff87
Christian <elchristog@gmail.com>
2023-11-19 19:16:37 +0000

    show_web_metrics	:earth_americas:

--- modules/seo_traffic_web.py
+++ modules/seo_traffic_web.py
@@ -400,7 +400,7 @@ def get_data_save_to_bq(role_id, project_url_clean):
 
 
 def show_web_metrics(project_name):
-    st.write("### Web traffic")
+    st.write("### 	:earth_americas: Web traffic")
     df = pd.DataFrame(uc.run_query_3_h(f"SELECT * FROM `company-data-driven.{project_name}.traffic_analytics_web_clicks` ORDER BY date ASC;"))
     df_grouped = df.groupby('date').agg({
             'clicks': 'sum',
