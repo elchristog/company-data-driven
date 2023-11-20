a76803c49c2b0e2af16d6a317af6f4f100ae786b
Christian <elchristog@gmail.com>
2023-11-20 22:21:43 +0000

    content_creation_guide text_input

--- modules/seo.py
+++ modules/seo.py
@@ -6,5 +6,16 @@ import datetime
 import utils.user_credentials as uc
 
 def content_creation_guide_effective_communication_storytelling():
-    st.success("Mi broti")
+    with st.form("comm_eff_storytelling_form"):
+
+        text_input = st.text_input(
+            "Enter some text 👇",
+            label_visibility=st.session_state.visibility,
+            disabled=st.session_state.disabled,
+            placeholder=st.session_state.placeholder,
+        )
+
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            st.write(text_input)
 
