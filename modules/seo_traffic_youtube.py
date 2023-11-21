68bb2e8438f33babda29d610e5f8dd9365040701
Christian <elchristog@gmail.com>
2023-11-21 19:21:53 +0000

    remove siteUrl=property_url, body=request

--- modules/seo_traffic_youtube.py
+++ modules/seo_traffic_youtube.py
@@ -111,7 +111,7 @@ def get_data_date(property_url, startDate, endDate, url_filter=None, url_operato
                     request['dimensionFilterGroups'].append({'filters': [palavra_dimension_filter]})
                 else:
                     request['dimensionFilterGroups'] = [{'filters': [palavra_dimension_filter]}]
-            response = service.reports().query(body=request).execute()
+            response = service.reports().query().execute()
             rows = response.get('rows', [])
             startRow = startRow + len(rows)
             data.extend(rows)
