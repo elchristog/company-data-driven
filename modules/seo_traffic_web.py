61e6722bc31b21609923bed7f3e849386eba209a
Christian <elchristog@gmail.com>
2023-11-21 19:02:24 +0000

    web # st.table(df_clicks)                 # st.table(df_pages)

--- modules/seo_traffic_web.py
+++ modules/seo_traffic_web.py
@@ -379,8 +379,8 @@ def get_data_save_to_bq(role_id, project_name, project_url_clean):
                 df_pages = get_data(property_url, ['page'], starting_date_to_pages, ending_date_to_pages,
                         url_filter=url_filter, url_operator=url_operator,
                         palavra_filter=palavra_filter, palavra_operator=palavra_operator)
-                st.table(df_clicks)
-                st.table(df_pages)
+                # st.table(df_clicks)
+                # st.table(df_pages)
                 today = datetime.date.today()
                 today_str = today.strftime("%Y-%m-%d")
                 st.info("Updating, please wait", icon = "☺️")
