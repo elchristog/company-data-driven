7d862093ea109712df8894ca5fa46aac2eb9e58d
Christian <elchristog@gmail.com>
2023-11-22 14:52:26 +0000

    views,averageViewDuration,subscribersGained

--- modules/seo_traffic_youtube.py
+++ modules/seo_traffic_youtube.py
@@ -102,7 +102,7 @@ def get_data_date(property_url, start_date, end_date, url_filter=None, url_opera
                 ids='channel==MINE',
                 startDate=start_date,
                 endDate=end_date,
-                metrics='views,averageViewDuration,viewerPercentage,subscribersGained',
+                metrics='views,averageViewDuration,subscribersGained',
                 dimensions='day',
                 sort='day'
             ).execute()
