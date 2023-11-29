88965c465784b66a39eebbe9398a4b8545515783
Christian <elchristog@gmail.com>
2023-11-29 18:46:57 +0000

    user_creation new countries

--- utils/users_handling.py
+++ utils/users_handling.py
@@ -80,7 +80,7 @@ def user_creation(user_id, project_id, project_name):
     user_birth_date = st.date_input("User birth date:", min_value = datetime.date(1970,1,1)) 
     user_country = st.selectbox(
         label = "Select user country",
-        options = ['colombia', 'united states', 'el salvador'],
+        options = ['colombia', 'united states', 'el salvador', 'mexico', 'venezuela', 'costa rica'],
         index = None
     )
     user_gender = st.selectbox(
