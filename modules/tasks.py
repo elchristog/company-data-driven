18cd379d0094774748be44e9769e6ed4803260eb
Christian <elchristog@gmail.com>
2023-11-30 15:44:59 +0000

    tasks_visualizer uc.run_query_2_m.clear

--- modules/tasks.py
+++ modules/tasks.py
@@ -8,7 +8,7 @@ import utils.chat_gpt_gestor as cgptg
 
 
 def tasks_visualizer(user_id, project_name, divider):
-    rows = uc.run_query_1_day(f"SELECT id, creation_date, description, commit_finish_date, status  FROM `company-data-driven.{project_name}.tasks` WHERE responsible_user_id = {user_id} AND status IN ('to_start', 'on_execution', 'delayed');") #finished, canceled, unfulfilled
+    rows = uc.run_query_2_m(f"SELECT id, creation_date, description, commit_finish_date, status  FROM `company-data-driven.{project_name}.tasks` WHERE responsible_user_id = {user_id} AND status IN ('to_start', 'on_execution', 'delayed');") #finished, canceled, unfulfilled
     if len(rows) == 0:
         st.success('You have no pending tasks, very good!', icon="😎")
     else:
@@ -62,11 +62,13 @@ def tasks_visualizer(user_id, project_name, divider):
                         selected_task_id = ids[descriptions.index(selected_task)]
                         update_task_status(selected_task_id, selected_status, today_str)
                         st.success('Task status updated!', icon="😎")
-                        uc.run_query_1_day.clear()
+                        uc.run_query_2_m.clear()
                         st.rerun()
     if divider == 1:
         st.write("---") 
     return rows
+
+
 
 
 def tasks_achievements(user_id, project_name, divider):
