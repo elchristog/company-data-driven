a4b2518c42588dc6d46abd45493a1a9aa6a830c4
Christian <elchristog@gmail.com>
2023-11-30 13:19:03 +0000

    query instant # @st.cache_data

--- utils/user_credentials.py
+++ utils/user_credentials.py
@@ -24,6 +24,7 @@ def run_query_insert_update_1_day(query):
     client.query(query)
 
 @st.cache_resource(show_spinner=False)
+# @st.cache_data(experimental_allow_widgets=True, show_spinner=False)
 def run_query_instant(query):
     query_job = client.query(query)
     rows_raw = query_job.result()
