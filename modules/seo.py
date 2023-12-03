490731bff5916ac3c5674c0606d828fd1d95ac6f
Christian <elchristog@gmail.com>
2023-12-03 07:02:25 -0500

    Update seo.py
    if 'answer' in st.session_state:
            st.download_button('Download answer', st.session_state.answer) identation back of form

--- modules/seo.py
+++ modules/seo.py
@@ -187,8 +187,8 @@ def seo_ideation(project_name, project_keyword, user_id, role_id):
         st.session_state.text_input_1 = text_input_1
         
         submitted = st.form_submit_button("Generate ideas", on_click = seo_ideation_execution)
-        if 'answer' in st.session_state:
-            st.download_button('Download answer', st.session_state.answer)
+    if 'answer' in st.session_state:
+        st.download_button('Download answer', st.session_state.answer)
     
     if role_id == 1:
         st.write("---")
