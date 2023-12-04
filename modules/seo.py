b6672ac1a92fd9aa7a686151bf8273dc7241c449
Christian <elchristog@gmail.com>
2023-12-04 15:32:23 -0500

    Update seo.py
    def seo_writing(project_name, user_id, role_id):
        non_created_contents = uc.run_query_30_m(f"SELECT * FROM `company-data-driven.{project_name}.effective_communication_content` WHERE created_content = 0 ORDER BY creation_date;")
        st.write(non_created_contents)

--- modules/seo.py
+++ modules/seo.py
@@ -251,6 +251,6 @@ def web_creation_guide():
 
 
 def seo_writing(project_name, user_id, role_id):
-    non_created_contents = uc.run_query_30_m(f"SELECT id, keyword FROM `company-data-driven.{project_name}.effective_communication_content` WHERE created_content = 0 ORDER BY creation_date;")
+    non_created_contents = uc.run_query_30_m(f"SELECT * FROM `company-data-driven.{project_name}.effective_communication_content` WHERE created_content = 0 ORDER BY creation_date;")
     st.write(non_created_contents)
 
