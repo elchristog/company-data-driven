813658e89ebffecfecd69159a96aeab7f949eb15
Christian <elchristog@gmail.com>
2023-12-08 08:22:21 -0500

    Update bitly.py
    st.write(df_bitly_web) identation

--- modules/bitly.py
+++ modules/bitly.py
@@ -72,7 +72,7 @@ def show_bitly_web_youtube_metrics(project_name):
     st.write("### 	:earth_americas: Bitly web conversion")
   
     df_bitly_web = pd.DataFrame(uc.run_query_1_h(f"SELECT tabc.date, tawc.clicks AS web_clicks, tabc.clicks AS bitly_clicks, ROUND(tabc.clicks/tawc.clicks, 2) AS conversion FROM `company-data-driven.{project_name}.traffic_analytics_bitly_clicks` AS tabc INNER JOIN `company-data-driven.{project_name}.traffic_analytics_web_clicks` AS tawc ON tabc.date = tawc.date WHERE tabc.bitly_link = 'bit.ly/3R6RbFW';"))
-  st.write(df_bitly_web)
+    st.write(df_bitly_web)
 
 
 
