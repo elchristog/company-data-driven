d6270774b672582e6b33195f944d833d4234637c
Christian <elchristog@gmail.com>
2023-12-15 13:52:09 -0500

    Update whatsapp_leads.py
      dates_bitly = uc.run_query_1_h(f"SELECT MIN(date) AS min_date_bitly, MAX(date) AS max_date_bitly FROM `company-data-driven.{project_name}.traffic_analytics_bitly_clicks`;")
      dates_whatsapp_leads = uc.run_query_1_h(f"SELECT MIN(creation_date) AS min_date_wsp, MAX(creation_date) AS max_date_wsp FROM `company-data-driven.{project_name}.traffic_analytics_whatsapp_leads`;")
      if len(dates_bitly) < 1 or len(dates_whatsapp_leads) < 1:
          st.warning("Waiting for data")
      else:
          day = st.date_input(
              "Time Range:",
              (np.maximum(dates_bitly[0].get('min_date_bitly'), dates_whatsapp_leads[0].get('min_date_wsp')), np.minimum(dates_bitly[0].get('max_date_bitly'), dates_whatsapp_leads[0].get('max_date_wsp'))),
              min_value=np.maximum(dates_bitly[0].get('min_date_bitly'), dates_whatsapp_leads[0].get('min_date_wsp')),
              max_value=np.minimum(dates_bitly[0].get('max_date_bitly'), dates_whatsapp_leads[0].get('max_date_wsp')),
              format="DD/MM/YYYY",
              help='',
              key = 'day_web'
          )

--- modules/whatsapp_leads.py
+++ modules/whatsapp_leads.py
@@ -5,4 +5,17 @@ import datetime
 import utils.user_credentials as uc
 
 def whatsapp_leads_show_metrics(project_name):
-  st.success("My Douglas")
+  dates_bitly = uc.run_query_1_h(f"SELECT MIN(date) AS min_date_bitly, MAX(date) AS max_date_bitly FROM `company-data-driven.{project_name}.traffic_analytics_bitly_clicks`;")
+  dates_whatsapp_leads = uc.run_query_1_h(f"SELECT MIN(creation_date) AS min_date_wsp, MAX(creation_date) AS max_date_wsp FROM `company-data-driven.{project_name}.traffic_analytics_whatsapp_leads`;")
+  if len(dates_bitly) < 1 or len(dates_whatsapp_leads) < 1:
+      st.warning("Waiting for data")
+  else:
+      day = st.date_input(
+          "Time Range:",
+          (np.maximum(dates_bitly[0].get('min_date_bitly'), dates_whatsapp_leads[0].get('min_date_wsp')), np.minimum(dates_bitly[0].get('max_date_bitly'), dates_whatsapp_leads[0].get('max_date_wsp'))),
+          min_value=np.maximum(dates_bitly[0].get('min_date_bitly'), dates_whatsapp_leads[0].get('min_date_wsp')),
+          max_value=np.minimum(dates_bitly[0].get('max_date_bitly'), dates_whatsapp_leads[0].get('max_date_wsp')),
+          format="DD/MM/YYYY",
+          help='',
+          key = 'day_web'
+      )
