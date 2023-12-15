b63fa22646a2abd6c16d2bdda991ebdae158fe32
Christian <elchristog@gmail.com>
2023-12-15 14:45:46 -0500

    Update whatsapp_leads.py
    st.write(df_conversion)
          bitly_clicks_total = df_conversion['bitly_clicks_total'].sum()
          num_leads_wsp = df_bitly_web['num_leads_wsp'].sum()
          conversion = num_leads_wsp/bitly_clicks_total
          met1, met2, met3 = st.columns(3)
          with met1:
              st.metric('bitly_clicks_total:', f'{bitly_clicks_total:,}')
          with met2:
              st.metric('num_leads_wsp:', f'{num_leads_wsp:,}')
          with met3:
              st.metric('conversion:', f'{conversion * 100:.2f}%')
          # with st.container():
          #     plot_echarts_btl_web_yt(df_bitly_web, 'web')

--- modules/whatsapp_leads.py
+++ modules/whatsapp_leads.py
@@ -29,15 +29,15 @@ def whatsapp_leads_show_metrics(project_name, bitly_web_link, bitly_yt_link):
       df_conversion = pd.DataFrame(uc.run_query_1_h(f"SELECT btl_totals.date, btl_totals.bitly_clicks_web, btl_totals.bitly_clicks_yt, btl_totals.bitly_clicks_total, wsp_leads.num_leads_wsp, ROUND(wsp_leads.num_leads_wsp/NULLIF(btl_totals.bitly_clicks_total, 0), 2) AS conversion  FROM (SELECT date, SUM(CASE WHEN bitly_link IN ('{bitly_web_link}') THEN clicks ELSE 0 END) AS bitly_clicks_web, SUM(CASE WHEN bitly_link IN ('{bitly_yt_link}') THEN clicks ELSE 0 END) AS bitly_clicks_yt, SUM(CASE WHEN bitly_link IN ('{bitly_web_link}', '{bitly_yt_link}') THEN clicks ELSE 0 END) AS bitly_clicks_total FROM `company-data-driven.{project_name}.traffic_analytics_bitly_clicks` GROUP BY date) AS btl_totals INNER JOIN (SELECT creation_date, COUNT(id) AS num_leads_wsp FROM `company-data-driven.{project_name}.traffic_analytics_whatsapp_leads` WHERE creation_date >= '{day[0].strftime('%Y-%m-%d')}'  AND  creation_date <= '{day[1].strftime('%Y-%m-%d')}'  GROUP BY creation_date) AS wsp_leads ON wsp_leads.creation_date = btl_totals.date ORDER BY btl_totals.date ASC;"))
     
       st.write(df_conversion)
-      # web_clicks = df_bitly_web['web_clicks'].sum()
-      # bitly_clicks = df_bitly_web['bitly_clicks'].sum()
-      # conversion = bitly_clicks/web_clicks
-      # met1, met2, met3 = st.columns(3)
-      # with met1:
-      #     st.metric('web_clicks:', f'{web_clicks:,}')
-      # with met2:
-      #     st.metric('bitly_clicks:', f'{bitly_clicks:,}')
-      # with met3:
-      #     st.metric('conversion:', f'{conversion * 100:.2f}%')
+      bitly_clicks_total = df_conversion['bitly_clicks_total'].sum()
+      num_leads_wsp = df_bitly_web['num_leads_wsp'].sum()
+      conversion = num_leads_wsp/bitly_clicks_total
+      met1, met2, met3 = st.columns(3)
+      with met1:
+          st.metric('bitly_clicks_total:', f'{bitly_clicks_total:,}')
+      with met2:
+          st.metric('num_leads_wsp:', f'{num_leads_wsp:,}')
+      with met3:
+          st.metric('conversion:', f'{conversion * 100:.2f}%')
       # with st.container():
       #     plot_echarts_btl_web_yt(df_bitly_web, 'web')
