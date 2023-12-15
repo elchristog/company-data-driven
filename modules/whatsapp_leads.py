33ef8f6398b7056537176a6f259f70c47ec79374
Christian <elchristog@gmail.com>
2023-12-15 16:38:57 -0500

    Update whatsapp_leads.py
    def whatsapp_leads_creation(user_id, project_name):
        st.warning("Do not use line breaks, double or single quotes, double or single quotes neither", icon = "🙈")
        with st.form("whatsapp_leads_creation_form", clear_on_submit = True):
            text_input_1 = st.text_area(
                "Keyword del articulo",
                label_visibility = 'visible',
                disabled = False,
                placeholder = 'Obtener la licencia de enfermería en Estados Unidos',
                help = 'No se debe poner cualquier cosa, debe ser lo que entrego el ideador',
                key = 'text_input_1'
            )
    
            submitted = st.form_submit_button("Submit", on_click = whatsapp_leads_creation_save, args = [project_name, user_id])

--- modules/whatsapp_leads.py
+++ modules/whatsapp_leads.py
@@ -136,3 +136,63 @@ def whatsapp_leads_show_metrics(project_name, bitly_web_link, bitly_yt_link):
           st.metric('bitly_clicks_web:', f'{bitly_clicks_web:,}')
       with met2:
           st.metric('bitly_clicks_yt:', f'{bitly_clicks_yt:,}')
+
+
+
+
+
+
+def whatsapp_leads_creation_save(project_name, user_id):
+    st.session_state.text_input_1 = re.sub('[^a-zA-Z0-9 \n\.]', ' ', st.session_state.text_input_1)
+    st.session_state.text_input_2 = re.sub('[^a-zA-Z0-9 \n\.]', ' ', st.session_state.text_input_2)
+    st.session_state.text_input_3 = re.sub('[^a-zA-Z0-9 \n\.]', ' ', st.session_state.text_input_3)
+    st.session_state.text_input_4 = re.sub('[^a-zA-Z0-9 \n\.]', ' ', st.session_state.text_input_4)
+    st.session_state.text_input_5 = re.sub('[^a-zA-Z0-9 \n\.]', ' ', st.session_state.text_input_5)
+    st.session_state.text_input_6 = re.sub('[^a-zA-Z0-9 \n\.]', ' ', st.session_state.text_input_6)
+    st.session_state.text_input_7 = re.sub('[^a-zA-Z0-9 \n\.]', ' ', st.session_state.text_input_7)
+    st.session_state.text_input_8 = re.sub('[^a-zA-Z0-9 \n\.]', ' ', st.session_state.text_input_8)
+    st.session_state.text_input_9 = re.sub('[^a-zA-Z0-9 \n\.]', ' ', st.session_state.text_input_9)
+    st.session_state.text_input_10 = re.sub('[^a-zA-Z0-9 \n\.]', ' ', st.session_state.text_input_10)
+    st.session_state.text_input_11 = re.sub('[^a-zA-Z0-9 \n\.]', ' ', st.session_state.text_input_11)
+    st.session_state.text_input_12 = re.sub('[^a-zA-Z0-9 \n\.]', ' ', st.session_state.text_input_12)
+    st.session_state.text_input_13 = re.sub('[^a-zA-Z0-9 \n\.]', ' ', st.session_state.text_input_13)
+    st.session_state.text_input_14 = re.sub('[^a-zA-Z0-9 \n\.]', ' ', st.session_state.text_input_14)
+
+    st.session_state.text_input_1 = re.sub(r"[\\\'\"']", " ", st.session_state.text_input_1)
+    st.session_state.text_input_2 = re.sub(r"[\\\'\"']", " ", st.session_state.text_input_2)
+    st.session_state.text_input_3 = re.sub(r"[\\\'\"']", " ", st.session_state.text_input_3)
+    st.session_state.text_input_4 = re.sub(r"[\\\'\"']", " ", st.session_state.text_input_4)
+    st.session_state.text_input_5 = re.sub(r"[\\\'\"']", " ", st.session_state.text_input_5)
+    st.session_state.text_input_6 = re.sub(r"[\\\'\"']", " ", st.session_state.text_input_6)
+    st.session_state.text_input_7 = re.sub(r"[\\\'\"']", " ", st.session_state.text_input_7)
+    st.session_state.text_input_8 = re.sub(r"[\\\'\"']", " ", st.session_state.text_input_8)
+    st.session_state.text_input_9 = re.sub(r"[\\\'\"']", " ", st.session_state.text_input_9)
+    st.session_state.text_input_10 = re.sub(r"[\\\'\"']", " ", st.session_state.text_input_10)
+    st.session_state.text_input_11 = re.sub(r"[\\\'\"']", " ", st.session_state.text_input_11)
+    st.session_state.text_input_12 = re.sub(r"[\\\'\"']", " ", st.session_state.text_input_12)
+    st.session_state.text_input_13 = re.sub(r"[\\\'\"']", " ", st.session_state.text_input_13)
+    st.session_state.text_input_14 = re.sub(r"[\\\'\"']", " ", st.session_state.text_input_14)
+    
+    
+    st.toast("Please wait", icon = "☺️")
+    # uc.run_query_insert_update(f"INSERT INTO `company-data-driven.{project_name}.effective_communication_content` (id, creation_date, creator_user_id, keyword, main_idea, why_1, why_2, why_3, how_1, how_2, how_3, experiment_1, experiment_2, experiment_3, relevant_content, checklist, call_to_action, created_content) VALUES (GENERATE_UUID(), CURRENT_DATE(), {user_id}, '{st.session_state.text_input_1}', '{st.session_state.text_input_2}', '{st.session_state.text_input_3}', '{st.session_state.text_input_4}', '{st.session_state.text_input_5}', '{st.session_state.text_input_6}', '{st.session_state.text_input_7}', '{st.session_state.text_input_8}', '{st.session_state.text_input_9}', '{st.session_state.text_input_10}', '{st.session_state.text_input_11}', '{st.session_state.text_input_12}', '{st.session_state.text_input_13}', '{st.session_state.text_input_14}' ,0);")
+    time.sleep(5)
+    st.toast("Content saved!", icon = "👾")
+    st.balloons()
+
+
+def whatsapp_leads_creation(user_id, project_name):
+    st.warning("Do not use line breaks, double or single quotes, double or single quotes neither", icon = "🙈")
+    with st.form("whatsapp_leads_creation_form", clear_on_submit = True):
+        text_input_1 = st.text_area(
+            "Keyword del articulo",
+            label_visibility = 'visible',
+            disabled = False,
+            placeholder = 'Obtener la licencia de enfermería en Estados Unidos',
+            help = 'No se debe poner cualquier cosa, debe ser lo que entrego el ideador',
+            key = 'text_input_1'
+        )
+
+        submitted = st.form_submit_button("Submit", on_click = whatsapp_leads_creation_save, args = [project_name, user_id])
+
+
