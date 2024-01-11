61a99daf1106634abc983b3925826984abf3e509
Christian <elchristog@gmail.com>
2024-01-11 13:58:15 -0500

    Update bitly_groupal_session.py
     num_leads_wsp = df_conversion['num_leads_wsp'].sum()
          num_groupal_session_clicks = df_conversion['num_groupal_session_clicks'].sum()
          conversion = num_groupal_session_clicks/num_leads_wsp
        
          met1, met2, met3 = st.columns(3)
          with met1:
              st.metric('num_leads_wsp:', f'{num_leads_wsp:,}')
          with met2:
              st.metric('num_groupal_session_clicks:', f'{num_groupal_session_clicks:,}')
          with met3:
              st.metric('conversion:', f'{conversion * 100:.2f}%')

--- modules/bitly_groupal_session.py
+++ modules/bitly_groupal_session.py
@@ -120,18 +120,16 @@ def bitly_groupal_session_show_metrics(project_name, bitly_groupal_session_link)
       df_conversion = pd.DataFrame(uc.run_query_1_h(f"SELECT groupal_session.date, groupal_session.num_groupal_session_clicks, whatsapp_leads.num_leads_wsp, ROUND(groupal_session.num_groupal_session_clicks/NULLIF(whatsapp_leads.num_leads_wsp, 0), 2) AS conversion FROM (SELECT date, SUM(clicks) AS num_groupal_session_clicks FROM `company-data-driven.{project_name}.traffic_analytics_bitly_clicks` WHERE bitly_link = '{bitly_groupal_session_link}' GROUP BY date) AS groupal_session INNER JOIN (SELECT creation_date, COUNT(id) AS num_leads_wsp FROM `company-data-driven.{project_name}.traffic_analytics_whatsapp_leads` WHERE creation_date >= '{day[0].strftime('%Y-%m-%d')}'  AND  creation_date <= '{day[1].strftime('%Y-%m-%d')}' GROUP BY creation_date) AS whatsapp_leads ON groupal_session.date = whatsapp_leads.creation_date ORDER BY groupal_session.date ASC;"))
       st.write(df_conversion)
     
-      # bitly_clicks_total = df_conversion['bitly_clicks_total'].sum()
-      # num_leads_wsp = df_conversion['num_leads_wsp'].sum()
-      # conversion = num_leads_wsp/bitly_clicks_total
-      # bitly_clicks_web = df_conversion['bitly_clicks_web'].sum()
-      # bitly_clicks_yt = df_conversion['bitly_clicks_yt'].sum()
+      num_leads_wsp = df_conversion['num_leads_wsp'].sum()
+      num_groupal_session_clicks = df_conversion['num_groupal_session_clicks'].sum()
+      conversion = num_groupal_session_clicks/num_leads_wsp
     
-      # met1, met2, met3 = st.columns(3)
-      # with met1:
-      #     st.metric('bitly_clicks_total:', f'{bitly_clicks_total:,}')
-      # with met2:
-      #     st.metric('num_leads_wsp:', f'{num_leads_wsp:,}')
-      # with met3:
-      #     st.metric('conversion:', f'{conversion * 100:.2f}%')
+      met1, met2, met3 = st.columns(3)
+      with met1:
+          st.metric('num_leads_wsp:', f'{num_leads_wsp:,}')
+      with met2:
+          st.metric('num_groupal_session_clicks:', f'{num_groupal_session_clicks:,}')
+      with met3:
+          st.metric('conversion:', f'{conversion * 100:.2f}%')
       # with st.container():
       #     plot_echarts_gsa(df_conversion)
