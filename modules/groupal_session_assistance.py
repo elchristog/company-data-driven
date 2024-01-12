a051b894cd1970ca6d784824e91134433ad33f5a
Christian <elchristog@gmail.com>
2024-01-12 16:47:01 -0500

    Update groupal_session_assistance.py
    groupal_session_link

--- modules/groupal_session_assistance.py
+++ modules/groupal_session_assistance.py
@@ -73,7 +73,7 @@ def plot_echarts_gsa(df_grouped):
 
 
 
-def groupal_session_show_metrics(project_name, bitly_web_link, bitly_yt_link):
+def groupal_session_show_metrics(project_name, bitly_groupal_session_link):
   dates_bitly = uc.run_query_1_h(f"SELECT MIN(date) AS min_date_bitly, MAX(date) AS max_date_bitly FROM `company-data-driven.{project_name}.traffic_analytics_bitly_clicks`;")
   dates_groupal_session_assistance = uc.run_query_1_h(f"SELECT MIN(meeting_date) AS min_date_gsa, MAX(meeting_date) AS max_date_gsa FROM `company-data-driven.{project_name}.traffic_analytics_groupal_session_assistance`;")
   if len(dates_bitly) < 1 or len(dates_groupal_session_assistance) < 1:
@@ -89,7 +89,7 @@ def groupal_session_show_metrics(project_name, bitly_web_link, bitly_yt_link):
           key = 'day_gsa'
       )
 
-      df_conversion = pd.DataFrame(uc.run_query_1_h(f"SELECT groupal_session_clicks_df.date, groupal_session_clicks_df.groupal_session_clicks, meeting_assistance.num_assistants FROM (SELECT meeting_date, COUNT(id) AS num_assistants FROM `company-data-driven.{project_name}.traffic_analytics_groupal_session_assistance` GROUP BY meeting_date) AS meeting_assistance RIGHT JOIN (SELECT date, SUM(clicks) AS groupal_session_clicks FROM `company-data-driven.{project_name}.traffic_analytics_bitly_clicks` WHERE bitly_link = 'bit.ly/3vtB3Wi' AND date >= '{day[0].strftime('%Y-%m-%d')}'  AND  date <= '{day[1].strftime('%Y-%m-%d')}'   GROUP BY date) AS groupal_session_clicks_df ON meeting_assistance.meeting_date = groupal_session_clicks_df.date ORDER BY groupal_session_clicks_df.date ASC;"))
+      df_conversion = pd.DataFrame(uc.run_query_1_h(f"SELECT groupal_session_clicks_df.date, groupal_session_clicks_df.groupal_session_clicks, meeting_assistance.num_assistants FROM (SELECT meeting_date, COUNT(id) AS num_assistants FROM `company-data-driven.{project_name}.traffic_analytics_groupal_session_assistance` GROUP BY meeting_date) AS meeting_assistance RIGHT JOIN (SELECT date, SUM(clicks) AS groupal_session_clicks FROM `company-data-driven.{project_name}.traffic_analytics_bitly_clicks` WHERE bitly_link = '{bitly_groupal_session_link}' AND date >= '{day[0].strftime('%Y-%m-%d')}'  AND  date <= '{day[1].strftime('%Y-%m-%d')}'   GROUP BY date) AS groupal_session_clicks_df ON meeting_assistance.meeting_date = groupal_session_clicks_df.date ORDER BY groupal_session_clicks_df.date ASC;"))
 
       st.write(df_conversion)
       bitly_clicks_groupal_session = df_conversion['groupal_session_clicks'].sum()
