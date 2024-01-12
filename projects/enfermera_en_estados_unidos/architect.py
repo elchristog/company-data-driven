019566d3e2d5ae30ce55476a1f02d5b3fd379b5a
Christian <elchristog@gmail.com>
2024-01-12 10:25:22 -0500

    Update architect.py
                    gsa.add_new_assistant(user_id, project_name)

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -147,7 +147,7 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
                 gsa.groupal_session_show_metrics(project_name, 'bit.ly/3R6RbFW', 'bit.ly/45SidF6')
             if sub_menu == "Add new assistant":
                 tap.title_and_paragraph("Add new assistant" + project_icon, "Seguimiento de los asistentes a la reunion grupal", "h3", 0)
-                gsa.add_new_assistant(project_name)
+                gsa.add_new_assistant(user_id, project_name)
 
 
 
