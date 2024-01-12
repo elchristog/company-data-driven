fd65dae4502fbbc2da9f34b347140146634773ed
Christian <elchristog@gmail.com>
2024-01-12 10:39:55 -0500

    Update groupal_session_assistance.py
    def add_new_assistant_execution(user_id, project_name, selected_phone_id):
        st.toast("Please wait", icon = "☺️")
        uc.run_query_insert_update(f"INSERT INTO `company-data-driven.{project_name}.traffic_analytics_groupal_session_assistance` (id, traffic_analytics_whatsapp_lead_id, meeting_date, creator_user_id) VALUES (GENERATE_UUID(), '{selected_phone_id}', CURRENT_DATE(), {user_id});")
        time.sleep(5)
        st.toast("Assistant saved!", icon = "👾")
        st.balloons()

--- modules/groupal_session_assistance.py
+++ modules/groupal_session_assistance.py
@@ -140,7 +140,11 @@ def groupal_session_show_metrics(project_name, bitly_web_link, bitly_yt_link):
 
 
 def add_new_assistant_execution(user_id, project_name, selected_phone_id):
-    st.toast(selected_phone_id)
+    st.toast("Please wait", icon = "☺️")
+    uc.run_query_insert_update(f"INSERT INTO `company-data-driven.{project_name}.traffic_analytics_groupal_session_assistance` (id, traffic_analytics_whatsapp_lead_id, meeting_date, creator_user_id) VALUES (GENERATE_UUID(), '{selected_phone_id}', CURRENT_DATE(), {user_id});")
+    time.sleep(5)
+    st.toast("Assistant saved!", icon = "👾")
+    st.balloons()
 
 
 
