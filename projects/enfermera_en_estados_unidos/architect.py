89cc956cbeb6d679cea90b571c9f267ccd4b501e
Christian <elchristog@gmail.com>
2024-01-15 14:52:11 -0500

    Update architect.py
    Seguimiento del trafico y los contenidos generados

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -79,7 +79,7 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
                 sub_menu_options.extend(['SEO ideation', 'SEO writting', 'Web creation guide'])
             sub_menu = st.sidebar.radio('Traffic options', options = sub_menu_options)
             if sub_menu == "Traffic":
-                tap.title_and_paragraph("Trafico" + project_icon, "Seguimiento y mantenimiento de los usuarios", "h3", 0)
+                tap.title_and_paragraph("Trafico" + project_icon, "Seguimiento del trafico y los contenidos generados", "h3", 0)
                 seotw.get_data_save_to_bq(role_id, project_name, project_url_clean)
                 seotw.show_web_metrics(project_name)
                 seoty.get_youtube_data_save_to_bq(role_id, project_name, project_url_clean)
