b06261c4371462147b2ec5f86c470ba14b55ae92
Christian <elchristog@gmail.com>
2024-01-20 08:56:02 -0500

    Update groupal_session_assistance.py
    team_member_contacts_week = team_member_contacts[(team_member_contacts["year_contact"].astype(str) == str(today.year)) & (team_member_contacts["month_contact"].astype(str) == str(today.month)) & (team_member_contacts["week_contact"].astype(str) == str(corrected_week))]
        # if len(team_member_contacts_week) < 1 or team_member_contacts_week is None < 0:
        #         st.warning(f"You have not added new contacts", icon = "🫥")
        # else:
        #     col1.metric(label="# Week Tests", value = team_member_contacts_week.shape[0])

--- modules/groupal_session_assistance.py
+++ modules/groupal_session_assistance.py
@@ -336,11 +336,11 @@ def groupal_session_team_member_performance(user_id, project_name):
     st.header("Week evolution")
     corrected_week = today.isocalendar()[1] + 1 if today.isocalendar()[2] == 7 else today.isocalendar()[1]
     col1, col2, col3, col4 = st.columns(4)
-    team_member_contacts_week = team_member_contacts[(team_member_contacts["year_contact"].astype(str) == str(today.year)) & (team_member_contacts["month_contact"].astype(str) == str(today.month)) & (team_member_contacts["week_contact"].astype(str) == str(corrected_week))]
-    if len(team_member_contacts_week) < 1 or team_member_contacts_week is None < 0:
-            st.warning(f"You have not added new contacts", icon = "🫥")
-    else:
-        col1.metric(label="# Week Tests", value = team_member_contacts_week.shape[0])
+    # team_member_contacts_week = team_member_contacts[(team_member_contacts["year_contact"].astype(str) == str(today.year)) & (team_member_contacts["month_contact"].astype(str) == str(today.month)) & (team_member_contacts["week_contact"].astype(str) == str(corrected_week))]
+    # if len(team_member_contacts_week) < 1 or team_member_contacts_week is None < 0:
+    #         st.warning(f"You have not added new contacts", icon = "🫥")
+    # else:
+    #     col1.metric(label="# Week Tests", value = team_member_contacts_week.shape[0])
         # col2.metric(label="# Avg score", value = round(team_member_contacts_week.score.mean(),0))
         # col3.metric(label="# % tests with score >= 80", value = str(round(100*(team_member_contacts_week['score'] >= 80).sum()/team_member_contacts_week.shape[0],1)))
         # col4.metric(label="# Avg days between tests", value = round(team_member_contacts_week.days_between_tests.mean(),1))
