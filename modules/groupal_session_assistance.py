9f3e5f2e49cbd4bf93608731b2da6075fb124e72
Christian <elchristog@gmail.com>
2024-01-20 08:45:44 -0500

    Update groupal_session_assistance.py
     st.header("Week evolution")
        corrected_week = today.isocalendar()[1] + 1 if today.isocalendar()[2] == 7 else today.isocalendar()[1]
        col1, col2, col3, col4 = st.columns(4)
        team_member_contacts_week = team_member_contacts[(team_member_contacts["year_attempt_date"] == today.year) & (team_member_contacts["month_attempt_date"] == today.month) & (team_member_contacts["week_attempt_date"] == corrected_week)]
        if len(team_member_contacts_week) < 1 or team_member_contacts_week is None < 0:
                st.warning(f"You have not added new contacts", icon = "🫥")
        else:
            col1.metric(label="# Week Tests", value = team_member_contacts_week.shape[0])

--- modules/groupal_session_assistance.py
+++ modules/groupal_session_assistance.py
@@ -329,4 +329,20 @@ def groupal_session_crm_user_view(project_name):
 def groupal_session_team_member_performance(user_id, project_name):
     team_member_contacts = uc.run_query_half_day(f"SELECT tagsc.contact_date, EXTRACT(YEAR FROM tagsc.contact_date) AS year_contact, EXTRACT(MONTH FROM tagsc.contact_date) AS month_contact, EXTRACT(WEEK FROM tagsc.contact_date) AS week_contact, tagsc.user_status FROM `company-data-driven.{project_name}.traffic_analytics_groupal_session_crm` AS tagsc WHERE tagsc.creator_id = {user_id} AND EXTRACT(YEAR FROM tagsc.contact_date) = EXTRACT(YEAR FROM CURRENT_DATE());")
     st.table(team_member_contacts)
+
+    today = datetime.date.today()
+    today_str = today.strftime("%Y-%m-%d")
+
+    st.header("Week evolution")
+    corrected_week = today.isocalendar()[1] + 1 if today.isocalendar()[2] == 7 else today.isocalendar()[1]
+    col1, col2, col3, col4 = st.columns(4)
+    team_member_contacts_week = team_member_contacts[(team_member_contacts["year_attempt_date"] == today.year) & (team_member_contacts["month_attempt_date"] == today.month) & (team_member_contacts["week_attempt_date"] == corrected_week)]
+    if len(team_member_contacts_week) < 1 or team_member_contacts_week is None < 0:
+            st.warning(f"You have not added new contacts", icon = "🫥")
+    else:
+        col1.metric(label="# Week Tests", value = team_member_contacts_week.shape[0])
+        # col2.metric(label="# Avg score", value = round(team_member_contacts_week.score.mean(),0))
+        # col3.metric(label="# % tests with score >= 80", value = str(round(100*(team_member_contacts_week['score'] >= 80).sum()/team_member_contacts_week.shape[0],1)))
+        # col4.metric(label="# Avg days between tests", value = round(team_member_contacts_week.days_between_tests.mean(),1))
+
     
