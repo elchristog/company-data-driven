9b210c462029f0ec5269f8daa75a20f52df80009
Christian <elchristog@gmail.com>
2024-02-02 18:29:17 -0500

    Update contracts.py
                col1, col2, col3, col4 = st.columns(4)
                col1.metric(label="Contract total value", value = contract_total_value)
                col2.metric(label="Total paid", value = total_paid)
                col3.metric(label="Current debt", value = current_debt)
                col4.metric(label="Last payment date", value = last_payment_date)

--- modules/contracts.py
+++ modules/contracts.py
@@ -413,9 +413,9 @@ def add_new_contract_payment(user_id, project_id, project_name):
             
             col1, col2, col3, col4 = st.columns(4)
             col1.metric(label="Contract total value", value = contract_total_value)
-            col1.metric(label="Total paid", value = total_paid)
-            col1.metric(label="Current debt", value = current_debt)
-            col1.metric(label="Last payment date", value = last_payment_date)
+            col2.metric(label="Total paid", value = total_paid)
+            col3.metric(label="Current debt", value = current_debt)
+            col4.metric(label="Last payment date", value = last_payment_date)
             
 
             payment_date = st.date_input("Payment date:", key = 'payment_date')
