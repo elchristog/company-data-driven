7db974c51429aaacb3b1185adbbfb232840e83f4
Christian <elchristog@gmail.com>
2024-02-04 09:32:31 -0500

    Update contracts.py
    def contract_payments_show_metrics(project_name):
      dates_payments = uc.run_query_1_h(f"SELECT MIN(payment_date) AS min_payment_date, MAX(payment_date) AS max_payment_date FROM `company-data-driven.{project_name}.contracts_payments` AS cp;")
      if len(dates_payments) < 1:
          st.warning("Waiting for data")
      else:
          day = st.date_input(
              "Time Range:",
              (dates_payments[0].get('min_payment_date'), dates_payments[0].get('max_payment_date')),
              min_value=dates_payments[0].get('min_payment_date'),
              max_value=dates_payments[0].get('max_payment_date'),
              format="DD/MM/YYYY",
              help='',
              key = 'day_payments'
          )

--- modules/contracts.py
+++ modules/contracts.py
@@ -338,36 +338,36 @@ def plot_echarts_contract_payments(df_grouped):
 
 
 def contract_payments_show_metrics(project_name):
-  dates_bitly = uc.run_query_1_h(f"SELECT MIN(date) AS min_date_bitly, MAX(date) AS max_date_bitly FROM `company-data-driven.{project_name}.traffic_analytics_bitly_clicks`;")
-  if len(dates_bitly) < 1:
+  dates_payments = uc.run_query_1_h(f"SELECT MIN(payment_date) AS min_payment_date, MAX(payment_date) AS max_payment_date FROM `company-data-driven.{project_name}.contracts_payments` AS cp;")
+  if len(dates_payments) < 1:
       st.warning("Waiting for data")
   else:
       day = st.date_input(
           "Time Range:",
-          (dates_bitly[0].get('min_date_bitly'), dates_bitly[0].get('max_date_bitly')),
-          min_value=dates_bitly[0].get('min_date_bitly'),
-          max_value=dates_bitly[0].get('max_date_bitly'),
+          (dates_payments[0].get('min_payment_date'), dates_payments[0].get('max_payment_date')),
+          min_value=dates_payments[0].get('min_payment_date'),
+          max_value=dates_payments[0].get('max_payment_date'),
           format="DD/MM/YYYY",
           help='',
-          key = 'day_gsa'
+          key = 'day_payments'
       )
 
-      df_conversion = pd.DataFrame(uc.run_query_1_h(f"SELECT groupal_session_clicks_df.date, groupal_session_clicks_df.groupal_session_clicks, meeting_assistance.num_assistants FROM (SELECT meeting_date, COUNT(id) AS num_assistants FROM `company-data-driven.{project_name}.traffic_analytics_groupal_session_assistance`  WHERE status = 'assistant' GROUP BY meeting_date) AS meeting_assistance RIGHT OUTER JOIN (SELECT date, SUM(clicks) AS groupal_session_clicks FROM `company-data-driven.{project_name}.traffic_analytics_bitly_clicks` WHERE bitly_link = '{bitly_groupal_session_link}' AND date >= '{day[0].strftime('%Y-%m-%d')}'  AND  date <= '{day[1].strftime('%Y-%m-%d')}'   GROUP BY date) AS groupal_session_clicks_df ON meeting_assistance.meeting_date = groupal_session_clicks_df.date ORDER BY groupal_session_clicks_df.date ASC;"))
+      # df_conversion = pd.DataFrame(uc.run_query_1_h(f"SELECT groupal_session_clicks_df.date, groupal_session_clicks_df.groupal_session_clicks, meeting_assistance.num_assistants FROM (SELECT meeting_date, COUNT(id) AS num_assistants FROM `company-data-driven.{project_name}.traffic_analytics_groupal_session_assistance`  WHERE status = 'assistant' GROUP BY meeting_date) AS meeting_assistance RIGHT OUTER JOIN (SELECT date, SUM(clicks) AS groupal_session_clicks FROM `company-data-driven.{project_name}.traffic_analytics_bitly_clicks` WHERE bitly_link = '{bitly_groupal_session_link}' AND date >= '{day[0].strftime('%Y-%m-%d')}'  AND  date <= '{day[1].strftime('%Y-%m-%d')}'   GROUP BY date) AS groupal_session_clicks_df ON meeting_assistance.meeting_date = groupal_session_clicks_df.date ORDER BY groupal_session_clicks_df.date ASC;"))
 
-      df_conversion["num_assistants"] = df_conversion["num_assistants"].fillna(0)
-      bitly_clicks_groupal_session = df_conversion['groupal_session_clicks'].sum()
-      num_assistants = df_conversion['num_assistants'].sum()
-      conversion = num_assistants/bitly_clicks_groupal_session
+      # df_conversion["num_assistants"] = df_conversion["num_assistants"].fillna(0)
+      # bitly_clicks_groupal_session = df_conversion['groupal_session_clicks'].sum()
+      # num_assistants = df_conversion['num_assistants'].sum()
+      # conversion = num_assistants/bitly_clicks_groupal_session
     
-      met1, met2, met3 = st.columns(3)
-      with met1:
-          st.metric('bitly_clicks_groupal_session:', f'{bitly_clicks_groupal_session:,}')
-      with met2:
-          st.metric('num_assistants:', f'{num_assistants:,}')
-      with met3:
-          st.metric('conversion:', f'{conversion * 100:.2f}%')
-      with st.container():
-          plot_echarts_contract_payments(df_conversion)
+      # met1, met2, met3 = st.columns(3)
+      # with met1:
+      #     st.metric('bitly_clicks_groupal_session:', f'{bitly_clicks_groupal_session:,}')
+      # with met2:
+      #     st.metric('num_assistants:', f'{num_assistants:,}')
+      # with met3:
+      #     st.metric('conversion:', f'{conversion * 100:.2f}%')
+      # with st.container():
+      #     plot_echarts_contract_payments(df_conversion)
 
 
 
