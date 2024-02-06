30bdd3096a2a2c2fde500d5fbb632bc32514cfe5
Christian <elchristog@gmail.com>
2024-02-06 07:30:32 -0500

    Create step_3_nursing_board_registration.py
    def cgfns_video_guide():
      video_file = open('https://drive.google.com/file/d/1QsqyOLueAlNMHsLgoDTkz1V9T_R6mK6L/view?usp=sharing', 'rb')
      video_bytes = video_file.read()
      
      st.video(video_bytes)

---
+++ projects/enfermera_en_estados_unidos/modules/step_3_nursing_board_registration.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def cgfns_video_guide():
+  video_file = open('https://drive.google.com/file/d/1QsqyOLueAlNMHsLgoDTkz1V9T_R6mK6L/view?usp=sharing', 'rb')
+  video_bytes = video_file.read()
+  
+  st.video(video_bytes)
