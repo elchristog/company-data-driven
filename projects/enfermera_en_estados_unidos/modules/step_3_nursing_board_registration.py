c11ba53cd7b0858b947a129beb9613e8fd1e47b1
Christian <elchristog@gmail.com>
2024-02-06 07:52:50 -0500

    Update step_3_nursing_board_registration.py
    def cgfns_video_guide():
      # video_file = open('https://drive.google.com/file/d/1QsqyOLueAlNMHsLgoDTkz1V9T_R6mK6L/view?usp=sharing', 'rb')
      # video_bytes = video_file.read()
      
      st.video('https://youtu.be/z77DLeDwc0E')

--- projects/enfermera_en_estados_unidos/modules/step_3_nursing_board_registration.py
+++ projects/enfermera_en_estados_unidos/modules/step_3_nursing_board_registration.py
@@ -4,4 +4,4 @@ def cgfns_video_guide():
   # video_file = open('https://drive.google.com/file/d/1QsqyOLueAlNMHsLgoDTkz1V9T_R6mK6L/view?usp=sharing', 'rb')
   # video_bytes = video_file.read()
   
-  st.video('https://drive.google.com/file/d/1QsqyOLueAlNMHsLgoDTkz1V9T_R6mK6L/view?usp=sharing')
+  st.video('https://youtu.be/z77DLeDwc0E')
