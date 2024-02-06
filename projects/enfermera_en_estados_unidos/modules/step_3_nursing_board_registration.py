ff8e70d9e52565a28f51dfec7d7783fe17172c65
Christian <elchristog@gmail.com>
2024-02-06 08:03:32 -0500

    Update step_3_nursing_board_registration.py
    markdown

--- projects/enfermera_en_estados_unidos/modules/step_3_nursing_board_registration.py
+++ projects/enfermera_en_estados_unidos/modules/step_3_nursing_board_registration.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def cgfns_video_guide():
-  st.write("** Crear CGFNS **")  
+  st.markdown("** Crear CGFNS **")  
   st.video('https://youtu.be/z77DLeDwc0E')
-  st.write("** Diligenciar CGFNS **")  
+  st.markdown("** Diligenciar CGFNS **")  
   st.video('https://www.youtube.com/watch?v=u1gfAGfvXio')
   
