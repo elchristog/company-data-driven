0ec874c676e96da030dd7eb3339fc5d7e39a0b6f
Christian <elchristog@gmail.com>
2024-02-10 11:19:23 -0500

    Update groupal_session_assistance.py
    SELECT 
               MAX(contact_date) AS last_contact_date,
               (SELECT user_status FROM `company-data-driven.{project_name}.traffic_analytics_groupal_session_crm` 
                 WHERE traffic_analytics_whatsapp_leads_id = '{selected_phone_id}'
                 ORDER BY contact_date DESC 
                 LIMIT 1) AS last_user_status
            FROM `company-data-driven.{project_name}.traffic_analytics_groupal_session_crm`
            WHERE traffic_analytics_whatsapp_leads_id = '{selected_phone_id}'

--- modules/groupal_session_assistance.py
+++ modules/groupal_session_assistance.py
@@ -239,12 +239,14 @@ def add_new_crm_groupal_session_contact_execution(user_id, project_name, selecte
 
    # Potentially Combined Query (If performant on your dataset)
     results =  uc.run_query_instant(f"""
-        SELECT 
-          MAX(contact_date) AS last_contact_date,
-          FIRST_VALUE(user_status) OVER (PARTITION BY traffic_analytics_whatsapp_leads_id ORDER BY contact_date DESC) AS last_user_status
+        SELECT 
+           MAX(contact_date) AS last_contact_date,
+           (SELECT user_status FROM `company-data-driven.{project_name}.traffic_analytics_groupal_session_crm` 
+             WHERE traffic_analytics_whatsapp_leads_id = '{selected_phone_id}'
+             ORDER BY contact_date DESC 
+             LIMIT 1) AS last_user_status
         FROM `company-data-driven.{project_name}.traffic_analytics_groupal_session_crm`
-        WHERE traffic_analytics_whatsapp_leads_id = '{selected_phone_id}' 
-        GROUP BY traffic_analytics_whatsapp_leads_id;
+        WHERE traffic_analytics_whatsapp_leads_id = '{selected_phone_id}'
     """)
 
     last_contact_date_value = results[0].get("last_contact_date") 
