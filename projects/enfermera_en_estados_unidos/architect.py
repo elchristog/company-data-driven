3c37353fdf83a3c176d6941ac015ba334cb1d364
Christian <elchristog@gmail.com>
2024-03-02 16:45:24 -0500

    Update architect.py
     if prompt:
                    st.session_state.prompt = prompt
                    st.session_state.ia_answer = ggg.gemini_knowledge_base_ia(project_name, "​Ahora soy un experto en el proceso de homologacion de enfermeria en estados unidos")
                if 'prompt' in st.session_state:
                    messages.chat_message("user").write(st.session_state.prompt)
                    messages.chat_message("assistant").write(f"Echo: {st.session_state.prompt}")

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -312,10 +312,10 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
 
         if menu == "IA questions":
             messages = st.container(height=300)
-            ggg.gemini_knowledge_base_ia(project_name)
             prompt = st.chat_input("Say something")                
             if prompt:
                 st.session_state.prompt = prompt
+                st.session_state.ia_answer = ggg.gemini_knowledge_base_ia(project_name, "​Ahora soy un experto en el proceso de homologacion de enfermeria en estados unidos")
             if 'prompt' in st.session_state:
                 messages.chat_message("user").write(st.session_state.prompt)
                 messages.chat_message("assistant").write(f"Echo: {st.session_state.prompt}")
