6f80defaac9b7039ba0b2e1a0edc0369f92198b1
Christian <elchristog@gmail.com>
2024-03-02 16:31:09 -0500

    Update g_gemini_gestor.py
    API_KEY=userdata.get(st.secrets["GEMINI_API_KEY"])
    genai.configure(api_key= API_KEY)
    
    def gemini_knowledge_base_ia():
        pass

--- utils/g_gemini_gestor.py
+++ utils/g_gemini_gestor.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import google.generativeai as genai
 
-openai.api_key = st.secrets["GEMINI_API_KEY"]
-
+API_KEY=userdata.get(st.secrets["GEMINI_API_KEY"])
+genai.configure(api_key= API_KEY)
 
 def gemini_knowledge_base_ia():
     pass
