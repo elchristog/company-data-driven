b03f5837d8a39bfbcb3d24a7f2a498c3aba0a893
Christian <elchristog@gmail.com>
2024-03-03 07:07:36 -0500

    Update bitly.py
    os.write(1, '🥏 Executing save_bitly_metrics_bulk \n'.encode('utf-8'))
      if 'save_bitly_metrics_bulk_days_last_update' not in st.session_state:
        dates_in_table = uc.run_query_half_day(f"SELECT CURRENT_DATE() AS current_date, DATE_DIFF(CURRENT_DATE(), MAX(date), DAY) AS days_last_update, MAX(date) AS max_date FROM `company-data-driven.{project_name}.traffic_analytics_bitly_clicks`;")
        current_date = dates_in_table[0].get('current_date')
        days_last_update = dates_in_table[0].get('days_last_update')
        max_date = dates_in_table[0].get('max_date')
        st.session_state.save_bitly_metrics_bulk_days_last_update = days_last_update
      else:
        days_last_update = st.session_state.save_bitly_metrics_bulk_days_last_update

--- modules/bitly.py
+++ modules/bitly.py
@@ -5,6 +5,7 @@ import requests
 import json
 import pandas as pd
 import numpy as np
+import os
 
 from datetime import datetime
 from streamlit_raw_echarts import st_echarts, JsCode
@@ -49,11 +50,17 @@ def save_bitly_metrics_one_link(project_name, bitly_link, link_name, max_stored_
 
 
 def save_bitly_metrics_bulk(project_name):
-  dates_in_table = uc.run_query_half_day(f"SELECT CURRENT_DATE() AS current_date, DATE_DIFF(CURRENT_DATE(), MAX(date), DAY) AS days_last_update, MAX(date) AS max_date FROM `company-data-driven.{project_name}.traffic_analytics_bitly_clicks`;")
-  current_date = dates_in_table[0].get('current_date')
-  days_last_update = dates_in_table[0].get('days_last_update')
-  max_date = dates_in_table[0].get('max_date')
+  os.write(1, '🥏 Executing save_bitly_metrics_bulk \n'.encode('utf-8'))
+  if 'save_bitly_metrics_bulk_days_last_update' not in st.session_state:
+    dates_in_table = uc.run_query_half_day(f"SELECT CURRENT_DATE() AS current_date, DATE_DIFF(CURRENT_DATE(), MAX(date), DAY) AS days_last_update, MAX(date) AS max_date FROM `company-data-driven.{project_name}.traffic_analytics_bitly_clicks`;")
+    current_date = dates_in_table[0].get('current_date')
+    days_last_update = dates_in_table[0].get('days_last_update')
+    max_date = dates_in_table[0].get('max_date')
+    st.session_state.save_bitly_metrics_bulk_days_last_update = days_last_update
+  else:
+    days_last_update = st.session_state.save_bitly_metrics_bulk_days_last_update
   if days_last_update is None or days_last_update is None:
+    os.write(1, '- save_bitly_metrics_bulk: Saving metrics \n'.encode('utf-8'))
     st.toast("Updating bitly data", icon = "🥶")
     save_bitly_metrics_one_link(project_name, 'bit.ly/45SidF6', 'enfermera_en_estados_unidos_youtube_to_whatsapp', None, current_date)
     save_bitly_metrics_one_link(project_name, 'bit.ly/3R6SrJa', 'enfermera_en_estados_unidos_instagram_to_whatsapp', None, current_date)
@@ -63,6 +70,7 @@ def save_bitly_metrics_bulk(project_name):
     save_bitly_metrics_one_link(project_name, 'bit.ly/3vtB3Wi', 'enfermera_en_estados_unidos_whatsapp_to_group_calendly', None, current_date)
     uc.run_query_half_day.clear()
   elif days_last_update > 1: # 1 day because today still getting data
+    os.write(1, '- save_bitly_metrics_bulk: Saving metrics \n'.encode('utf-8'))
     st.toast("Updating bitly data", icon = "🥶")
     save_bitly_metrics_one_link(project_name, 'bit.ly/45SidF6', 'enfermera_en_estados_unidos_youtube_to_whatsapp', max_date, current_date)
     save_bitly_metrics_one_link(project_name, 'bit.ly/3R6SrJa', 'enfermera_en_estados_unidos_instagram_to_whatsapp', max_date, current_date)
@@ -72,6 +80,7 @@ def save_bitly_metrics_bulk(project_name):
     save_bitly_metrics_one_link(project_name, 'bit.ly/3vtB3Wi', 'enfermera_en_estados_unidos_whatsapp_to_group_calendly', max_date, current_date)
     uc.run_query_half_day.clear()
   else:
+    os.write(1, '- save_bitly_metrics_bulk: Metrics already saved \n'.encode('utf-8'))
     pass
 
 
