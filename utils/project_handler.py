318923e3cd6d10f383df7ba8d35a0a392ae1f3ed
Christian <elchristog@gmail.com>
2024-03-03 06:49:26 -0500

    Update project_handler.py
        os.write(1, '🥏 Executing project_handler \n'.encode('utf-8'))

--- utils/project_handler.py
+++ utils/project_handler.py
@@ -2,6 +2,7 @@ import streamlit as st
 import projects.enfermera_en_estados_unidos.architect as eeeu_architect
 
 def project_handler(user_id, project_id, role_id, role_name, project_name, project_title, project_icon, project_logo_url, project_url_clean, project_keyword):
-    # enfermera_en_estados_unidos
+    os.write(1, '🥏 Executing project_handler \n'.encode('utf-8'))
     if project_id == 1:
+        os.write(1, '- project_handler: enfermera_en_estados_unidos \n'.encode('utf-8'))
         eeeu_architect.architect(user_id, role_id, project_id, project_name, project_title, project_icon, project_logo_url, project_url_clean, project_keyword)
