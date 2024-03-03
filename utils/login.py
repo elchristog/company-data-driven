17a8fdbbc49ca1cd694bad7efd367a9931cf87a5
Christian <elchristog@gmail.com>
2024-03-03 06:20:29 -0500

    Update login.py
                # with st.sidebar:
                #     st.write("---") 
                #     authenticator.logout('Logout', 'main')
            if st.session_state["authentication_status"] is False:
                st.error('Username/password is incorrect (Ask to the admin if is hashed)')

--- utils/login.py
+++ utils/login.py
@@ -8,7 +8,7 @@ import utils.user_credentials as uc
 
 def login():
     os.write(1, '🥏 Executing login \n'.encode('utf-8'))
-    if 'authentication_status' not in st.session_state or st.session_state["authentication_status"] is None:
+    if 'authentication_status' not in st.session_state or st.session_state["authentication_status"] is None or st.session_state["authentication_status"] is False:
         os.write(1, '- login: Authenticating user \n'.encode('utf-8'))
         with open('config.yaml') as file:
             config = yaml.load(file, Loader=SafeLoader)
@@ -26,9 +26,9 @@ def login():
             # with st.sidebar:
             #     st.write("---") 
             #     authenticator.logout('Logout', 'main')
+        if st.session_state["authentication_status"] is False:
+            st.error('Username/password is incorrect (Ask to the admin if is hashed)')
     if st.session_state["authentication_status"]:
         os.write(1, '- login: Skipping authentication \n'.encode('utf-8'))
         uc.user_credentials(st.session_state.name, st.session_state.authentication_status, st.session_state.username)
         st.write("---") 
-    elif st.session_state["authentication_status"] is False:
-        st.error('Username/password is incorrect (Ask to the admin if is hashed)')
