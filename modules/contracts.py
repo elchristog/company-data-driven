e099f7b3812f884c4f470fe6bfa55445c8e64166
Christian <elchristog@gmail.com>
2024-03-03 09:13:16 -0500

    Update contracts.py
    if 'checking_username_query_customer_creation' not in st.session_state:
            checking_username_query = uc.run_query_30_m(f"SELECT id FROM `company-data-driven.global.users` WHERE username = '{username}';")
            st.session_state.checking_username_query_customer_creation = checking_username_query
        else:
            checking_username_query = st.session_state.checking_username_query_customer_creation

--- modules/contracts.py
+++ modules/contracts.py
@@ -199,12 +199,18 @@ def customer_creation(user_id_customer_creation, project_id, project_name):
     else:
         username = st.text_input("Write the username:", st.session_state.username_customer_creation)
         st.session_state.username_customer_creation = username
+
+    if 'checking_username_query_customer_creation' not in st.session_state:
+        checking_username_query = uc.run_query_30_m(f"SELECT id FROM `company-data-driven.global.users` WHERE username = '{username}';")
+        st.session_state.checking_username_query_customer_creation = checking_username_query
+    else:
+        checking_username_query = st.session_state.checking_username_query_customer_creation
     
-    checking_username_query = uc.run_query_30_m(f"SELECT id FROM `company-data-driven.global.users` WHERE username = '{username}';")
     if len(checking_username_query) > 0 or len(username) < 6 or username is None:
         st.error('Username is not available', icon = '👻')
     else:
         st.success('Username available', icon = '🪬')
+        
     max_id_users = uc.run_query_instant(f"SELECT 1 + MAX(id) AS max_id FROM `company-data-driven.global.users`;")[0].get('max_id')
     max_id_role_assignement = uc.run_query_instant(f"SELECT 1 + MAX(id) AS max_id FROM `company-data-driven.global.role_assignment`;")[0].get('max_id')
 
