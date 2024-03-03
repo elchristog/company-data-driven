4b5daa423e98c22f45937d3f3d0d8a718e51150a
Christian <elchristog@gmail.com>
2024-03-03 06:05:00 -0500

    Update login.py
            os.write(1, '- login: Skipping authentication \n'.encode('utf-8'))

--- utils/login.py
+++ utils/login.py
@@ -8,24 +8,26 @@ import utils.user_credentials as uc
 
 def login():
     os.write(1, '🥏 Executing login \n'.encode('utf-8'))
-    with open('config.yaml') as file:
-        config = yaml.load(file, Loader=SafeLoader)
-        authenticator = stauth.Authenticate(
-            config['credentials'],
-            config['cookie']['name'],
-            config['cookie']['key'],
-            config['cookie']['expiry_days'],
-            config['preauthorized']
-        )
-        name, authentication_status, username = authenticator.login()
+    if 'authentication_status' not in st.session_state:
         os.write(1, '- login: Authenticating user \n'.encode('utf-8'))
-        if st.session_state["authentication_status"]:
-            uc.user_credentials(name, authentication_status, username)
+        with open('config.yaml') as file:
+            config = yaml.load(file, Loader=SafeLoader)
+            authenticator = stauth.Authenticate(
+                config['credentials'],
+                config['cookie']['name'],
+                config['cookie']['key'],
+                config['cookie']['expiry_days'],
+                config['preauthorized']
+            )
+            name, authentication_status, username = authenticator.login()
+    if st.session_state["authentication_status"]:
+        os.write(1, '- login: Skipping authentication \n'.encode('utf-8'))
+        uc.user_credentials(name, authentication_status, username)
+        st.write("---") 
+        with st.sidebar:
             st.write("---") 
-            with st.sidebar:
-                st.write("---") 
-                authenticator.logout('Logout', 'main')
-        elif st.session_state["authentication_status"] is False:
-            st.error('Username/password is incorrect (Ask to the admin if is hashed)')
-        elif st.session_state["authentication_status"] is None:
-            st.warning('Please enter your username and password')
+            authenticator.logout('Logout', 'main')
+    elif st.session_state["authentication_status"] is False:
+        st.error('Username/password is incorrect (Ask to the admin if is hashed)')
+    elif st.session_state["authentication_status"] is None:
+        st.warning('Please enter your username and password')
