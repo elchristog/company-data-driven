24dc8b8c0d53b0ca3ecdc9f555b1002222a2c954
Christian <elchristog@gmail.com>
2024-03-07 14:07:28 -0500

    Update step_4_nclex_preparation.py
    deleted    user confirmation

--- projects/enfermera_en_estados_unidos/modules/step_4_nclex_preparation.py
+++ projects/enfermera_en_estados_unidos/modules/step_4_nclex_preparation.py
@@ -39,18 +39,12 @@ def study_plan(user_id, project_id, project_name):
       st.session_state.study_plan_selected_username = selected_username
       if st.session_state.study_plan_selected_username is not None:
             st.session_state.study_plan_selected_username_index  = usernames.index(st.session_state.study_plan_selected_username)
-    
-  
-  checking_username = uc.run_query_30_m(f"SELECT id FROM `company-data-driven.global.users` WHERE username = '{selected_username}';")
-  if len(checking_username) < 1 or checking_username is None:
-      st.error('User does not exists', icon = '👻')
-  else:
-      st.success('User confirmed!', icon = '🪬')
-      if selected_username is not None:
-          study_plan_user_id = user_id
-          study_plan_project_id = project_id
-          study_plan_project_name = project_name
-          study_plan_selected_user_id = ids[usernames.index(selected_username)]
-          study_plan_selected_contract_id = contract_ids[usernames.index(selected_username)]
-          study_plan_button = st.button("Create Study plan", on_click = study_plan_execution, args = [study_plan_user_id, study_plan_project_id, study_plan_project_name, study_plan_selected_user_id, study_plan_selected_contract_id])
+
+    if selected_username is not None:
+        study_plan_user_id = user_id
+        study_plan_project_id = project_id
+        study_plan_project_name = project_name
+        study_plan_selected_user_id = ids[usernames.index(selected_username)]
+        study_plan_selected_contract_id = contract_ids[usernames.index(selected_username)]
+        study_plan_button = st.button("Create Study plan", on_click = study_plan_execution, args = [study_plan_user_id, study_plan_project_id, study_plan_project_name, study_plan_selected_user_id, study_plan_selected_contract_id])
 
