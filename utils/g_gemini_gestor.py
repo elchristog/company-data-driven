97ca5b9a56a144a909616cc234c92e4c72f89208
Christian <elchristog@gmail.com>
2024-03-07 12:46:39 -0500

    Update g_gemini_gestor.py
    def gemini_general_prompt(knowledge_prompt, model_prompt, user_question):
        os.write(1, '🥏 Executing gemini_general_prompt \n'.encode('utf-8'))
        generation_config = {
          "temperature": 0.9,
          "top_p": 1,
          "top_k": 1,
          "max_output_tokens": 2048,
        }
        
        safety_settings = [
          {
            "category": "HARM_CATEGORY_HARASSMENT",
            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
          },
          {
            "category": "HARM_CATEGORY_HATE_SPEECH",
            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
          },
          {
            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
          },
          {
            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
          },
        ]
        
        model = genai.GenerativeModel(model_name="gemini-1.0-pro",
                                      generation_config=generation_config,
                                      safety_settings=safety_settings)
        
        convo = model.start_chat(history=[
          {
            "role": "user",
            "parts": [knowledge_prompt]
          },
          {
            "role": "model",
            "parts": [model_prompt]
          },
        ])
        
        convo.send_message(user_question)
        os.write(1, '- gemini_general_prompt: Generating answer \n'.encode('utf-8'))
        return convo.last.text

--- utils/g_gemini_gestor.py
+++ utils/g_gemini_gestor.py
@@ -56,3 +56,57 @@ def gemini_knowledge_base_ia(project_name, model_prompt, user_question):
     convo.send_message(user_question)
     os.write(1, '- gemini_knowledge_base_ia: Generating answer \n'.encode('utf-8'))
     return convo.last.text
+
+
+
+
+
+
+
+
+def gemini_general_prompt(knowledge_prompt, model_prompt, user_question):
+    os.write(1, '🥏 Executing gemini_general_prompt \n'.encode('utf-8'))
+    generation_config = {
+      "temperature": 0.9,
+      "top_p": 1,
+      "top_k": 1,
+      "max_output_tokens": 2048,
+    }
+    
+    safety_settings = [
+      {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+      },
+      {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+      },
+      {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+      },
+      {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+      },
+    ]
+    
+    model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                                  generation_config=generation_config,
+                                  safety_settings=safety_settings)
+    
+    convo = model.start_chat(history=[
+      {
+        "role": "user",
+        "parts": [knowledge_prompt]
+      },
+      {
+        "role": "model",
+        "parts": [model_prompt]
+      },
+    ])
+    
+    convo.send_message(user_question)
+    os.write(1, '- gemini_general_prompt: Generating answer \n'.encode('utf-8'))
+    return convo.last.text
