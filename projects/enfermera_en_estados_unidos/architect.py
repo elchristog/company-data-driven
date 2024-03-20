614a7c948292f353b362d49543bcc1c4a207e3d6
Christian <elchristog@gmail.com>
2024-03-20 11:38:29 -0400

    Update architect.py
     st.write("---")
                    st.video("https://youtu.be/rwa_VGAQ_XE")

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -120,6 +120,8 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
             if sub_menu == "Video edition":
                 tap.title_and_paragraph("Edicion de video" + project_icon, "Marcar videos que:  1- ya he Editado, 2- subido a la carpeta de drive (https://drive.google.com/drive/folders/1oIbnhPISgGNG80TNQCqFA_1lm46Dxu2e?usp=drive_link) 3 - he creado la portada 4 - he notificado en el grupo de Whatsapp que ya esta fue editado", "h3", 0)
                 seo.video_edition(user_id, project_name)
+                st.write("---")
+                st.video("https://youtu.be/rwa_VGAQ_XE")
 
             if sub_menu == "Video uploading":
                 tap.title_and_paragraph("Carga de video" + project_icon, "Marcar videos que: 1- ya he Cargado a Youtube 2- ya cargue a Facebook 3- he usando correctamente el titulo generado, descripcion y etiquetas generadas, 4- Agregado a su lista de reproduccion, agregadas tarjetas y videos finales, 5- agregado el comentario fijado", "h3", 0)
