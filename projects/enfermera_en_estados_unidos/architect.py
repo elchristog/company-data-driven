3800cfd9eeba8667dad7aeb105c848e0a015cfb0
Christian <elchristog@gmail.com>
2024-03-20 17:52:14 -0400

    Update architect.py
     if sub_menu == "Video to shorts":
                    tap.title_and_paragraph("Creacion de shorts y carga" + project_icon, "Marcar videos que: 1- He creado los shorts usando Capcut 2- he subido cada short usando correctamente el titulo, descripcion y etiquetas 3- los he subido todos a youtube, instagram, facebook, Whatsapp", "h3", 0)
                    seo.video_to_shorts(user_id, project_name)
                    st.write("---")
                    st.video("https://www.youtube.com/watch?v=kze_LOZzVAI")

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -132,6 +132,8 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
             if sub_menu == "Video to shorts":
                 tap.title_and_paragraph("Creacion de shorts y carga" + project_icon, "Marcar videos que: 1- He creado los shorts usando Capcut 2- he subido cada short usando correctamente el titulo, descripcion y etiquetas 3- los he subido todos a youtube, instagram, facebook, Whatsapp", "h3", 0)
                 seo.video_to_shorts(user_id, project_name)
+                st.write("---")
+                st.video("https://www.youtube.com/watch?v=kze_LOZzVAI")
 
             if sub_menu == "Web creation":
                 tap.title_and_paragraph("Web creation" + project_icon, "1- Traer el transcript del video 2- clonar una pagina existente 3- Edicion rapida y nombrar 4- crear titulo, focus keyphrase, seo title, meta descripcion 5- edita en la pagina el titulo, meta descripcion, convierte a html agregando el resto cuidando de no volver a agregar ningun H1, aseguando que incluya 3 tablas comparativas y 3 estadisticas convincentes, justifica el texto 6- agrega el video asociado y repite el paso anterior con la segunda parte del articulo sin nuevos H1, nunca incluir las partes de body ni html 7- publicar 8- escribir en el grupo solicitando indexar y medir velocidad", "h3", 0)
