c06c64dc7ed9b62e71667f77088dc9aec1357e9c
Christian <elchristog@gmail.com>
2024-03-22 06:50:40 -0400

    Update architect.py
                    st.video("https://www.youtube.com/watch?v=5RitVZPPyK4")

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -159,10 +159,10 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
                 st.video("https://youtu.be/BRVyx_UG3MU")
 
             if sub_menu == "Post to web":
-                tap.title_and_paragraph("Post to web" + project_icon, "1- Traer el transcript del video 2- clonar una pagina existente 3- Edicion rapida y nombrar 4- crear titulo, focus keyphrase, seo title, meta descripcion 5- edita en la pagina el titulo, meta descripcion, convierte a html agregando el resto cuidando de no volver a agregar ningun H1, aseguando que incluya 3 tablas comparativas y 3 estadisticas convincentes, justifica el texto 6- agrega el video asociado y repite el paso anterior con la segunda parte del articulo sin nuevos H1, nunca incluir las partes de body ni html 7- publicar 8- escribir en el grupo solicitando indexar y medir velocidad", "h3", 0)
+                tap.title_and_paragraph("Post to web" + project_icon, "1- Seleccionar la idea 2- clonar una pagina existente 3- Edicion rapida y nombrar 4- crear titulo, focus keyphrase, seo title, meta descripcion 5- edita en la pagina el titulo, meta descripcion, convierte a html agregando el resto cuidando de no volver a agregar ningun H1, aseguando que incluya 3 tablas comparativas y 3 estadisticas convincentes, justifica el texto 6- agrega el video asociado y repite el paso anterior con la segunda parte del articulo sin nuevos H1, nunca incluir las partes de body ni html 7- publicar 8- escribir en el grupo solicitando indexar y medir velocidad", "h3", 0)
                 seo.post_to_web(user_id, project_name, project_keyword)
                 st.write("---")
-                st.video("https://www.youtube.com/watch?v=3Gfs7YGKSEU&ab_channel=EnfermeraenEstadosUnidos")
+                st.video("https://www.youtube.com/watch?v=5RitVZPPyK4")
                 st.write("convierte a html, tambien agrega 3 tablas comparativas y 3 estadisticas convincentes, agregalas de forma amigable en distintas partes del contenido en lugar de todo junto:")
                 st.write("resume el articulo en 2 consejos en un solo parrafo muy corto de maximo 140 caracteres, pero en un solo parrafo,  además Asegúrate que incluye la frase de forma natural: como ser enfermero en estados unidos")
 
