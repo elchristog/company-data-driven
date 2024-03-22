488c9d1a50a02e1426e17c10198678978b3e4f8d
Christian <elchristog@gmail.com>
2024-03-22 07:11:48 -0400

    Update architect.py
        if user_id == 31: #trafficker 01 - juancho
                    sub_menu_options = ["Traffic", "SEO ideation", 'Video uploading', 'Video to shorts', 'Web creation', 'Posting posts', 'Post to web']

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -92,6 +92,8 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
                 sub_menu_options=['Traffic', 'SEO ideation', 'Video creation', 'Video edition', 'Video uploading', 'Video to shorts', 'Web creation', 'Post idea creation', 'Posting posts', 'Post to web']
             if user_id == 2: #flaca
                 sub_menu_options=['Traffic', 'Video creation', 'Post idea creation']
+            if user_id == 31: #trafficker 01 - juancho
+                sub_menu_options = ["Traffic", "SEO ideation", 'Video uploading', 'Video to shorts', 'Web creation', 'Posting posts', 'Post to web']
            #------ Sub Menu according to each user-----------
             
             if role_id == 1:
