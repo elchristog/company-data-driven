caf33df7d8e029982589945c63f916604df06ee9
Christian <elchristog@gmail.com>
2024-04-04 12:01:01 -0400

    Update program_progress.py
            for each_task in all_tasks:
                st.toast(each_task)

--- modules/program_progress.py
+++ modules/program_progress.py
@@ -104,7 +104,8 @@ def customer_success_crm_add_contact_execution():
         today = datetime.date.today()
         today_str = today.strftime("%Y-%m-%d")
         all_tasks = st.session_state.customer_success_crm_add_contact_tasks.split(",")
-        st.toast(all_tasks)
+        for each_task in all_tasks:
+            st.toast(each_task)
         
         # max_id =  uc.run_query_instant(f"SELECT MAX(id)+1 AS max_id FROM `company-data-driven.{st.session_state.customer_success_crm_add_contact_project_name}.tasks`")[0].get('max_id')
         # uc.run_query_insert_update(f"INSERT INTO `company-data-driven.{st.session_state.customer_success_crm_add_contact_project_name}.tasks` (id, creation_date, description, responsible_user_id, commit_finish_date, status, task_creator_id) VALUES({max_id}, '{today_str}', '{st.session_state.task_input}', {st.session_state.selected_user_id}, '{st.session_state.commitment_date_input}', 'to_start', {st.session_state.user_id})")
