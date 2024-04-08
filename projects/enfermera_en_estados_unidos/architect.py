08c0bda6432e6cf86a5b213173cec56230f209c6
Christian <elchristog@gmail.com>
2024-04-08 16:34:53 -0400

    Update architect.py
                    pp.users_to_contact(project_name)

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -328,6 +328,7 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
             sub_menu = st.sidebar.radio('Customer success options', options = sub_menu_options)
             if sub_menu == "CRM":
                 tap.title_and_paragraph("CRM" + project_icon, "Seguimiento del contacto a clientes", "h3", 0)
+                pp.users_to_contact(project_name)
                 pp.customer_success_crm_add_contact(user_id, project_name)
             if sub_menu == "Add program step":
                 tap.title_and_paragraph("Add program step" + project_icon, "Agregar pasos al programa", "h3", 0)
