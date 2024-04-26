da402f5f95558e9f96f9d060370da7cd2cf6173e
Christian <elchristog@gmail.com>
2024-04-26 05:02:59 -0400

    Update architect.py
     st.write(" Queremos darte la bienvenida al programa de homologación de enfermería en Estados Unidos.    Vamos a iniciar con los 2 pasos iniciales: El primero de ellos es que programes tu reunión inicial en el momento que mejor se te ajuste. Aquí vamos a evaluar tu caso y a definir las primeras tareas: https://calendly.com/enfermeraenestadosunidos/reunion-inicial-1    Y el segundo paso es que puedas ir completando este formulario con la información que usaremos para crearte tus credenciales en las distintas plataformas: https://forms.gle/Qn6tMTay9KmGDx8k6 Muchas gracias y quedamos atentos a cualquier duda que tengas. ")

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -356,6 +356,7 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
                 with col1:
                     st.write("#### Inicio del programa")
                     st.video("https://www.youtube.com/watch?v=yx8Dyk68MKk")
+                    st.write(" Queremos darte la bienvenida al programa de homologación de enfermería en Estados Unidos.    Vamos a iniciar con los 2 pasos iniciales: El primero de ellos es que programes tu reunión inicial en el momento que mejor se te ajuste. Aquí vamos a evaluar tu caso y a definir las primeras tareas: https://calendly.com/enfermeraenestadosunidos/reunion-inicial-1    Y el segundo paso es que puedas ir completando este formulario con la información que usaremos para crearte tus credenciales en las distintas plataformas: https://forms.gle/Qn6tMTay9KmGDx8k6 Muchas gracias y quedamos atentos a cualquier duda que tengas. ")
                 with col2:
                     st.write("#### User credentials")
                     st.video("https://www.youtube.com/watch?v=ChAMEAnXSi8")
