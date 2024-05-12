a81c189c2266f6fe9242a7f24e80c61b6ffa038b
Christian <elchristog@gmail.com>
2024-05-12 14:49:41 -0400

    Update step_4_nclex_preparation.py
        guide_name = st.text_input("Guide name: ", key = "add_study_guide_guide_name")

--- projects/enfermera_en_estados_unidos/modules/step_4_nclex_preparation.py
+++ projects/enfermera_en_estados_unidos/modules/step_4_nclex_preparation.py
@@ -266,7 +266,7 @@ def add_study_guide(user_id, project_name):
             key= "add_study_guide_selected_folder"
         )
 
-  guide_name = st.text_input("Guide name: ", key = "add_study_guide_guide_name")
+    guide_name = st.text_input("Guide name: ", key = "add_study_guide_guide_name")
   
     subjects_rows = uc.run_query_half_day(f"SELECT DISTINCT(subject) AS subject FROM `company-data-driven.{project_name}.nclex_questions`;")
     subjects = []
