d7881dce4009bcc9195f2db95ad75a9b06ad35c3
Christian <elchristog@gmail.com>
2024-05-12 14:27:33 -0400

    Update architect.py
                if sub_menu == "Add study guide":
                    tap.title_and_paragraph("Add study guide " + project_icon, ".", "h3", 0)
                    s4np.add_question_to_test(user_id, project_name)

--- projects/enfermera_en_estados_unidos/architect.py
+++ projects/enfermera_en_estados_unidos/architect.py
@@ -329,7 +329,7 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
         
 
         if menu == "Web App":
-            sub_menu_options=["Customer view", 'Nclex test creation']
+            sub_menu_options=["Customer view", 'Nclex test creation', 'Add study guide']
             if role_id < 4:
                 sub_menu_options.extend(['Team view'])
             sub_menu = st.sidebar.radio('Web tool options', options = sub_menu_options)
@@ -342,6 +342,9 @@ def architect(user_id, role_id, project_id, project_name, project_title, project
             if sub_menu == "Nclex test creation":
                 tap.title_and_paragraph("Create a new question in the NCLEX test " + project_icon, "Remember not add the letters [A), B), C), D)] in the options", "h3", 0)
                 tst.add_question_to_test(project_name, 'nclex_questions', user_id)
+            if sub_menu == "Add study guide":
+                tap.title_and_paragraph("Add study guide " + project_icon, ".", "h3", 0)
+                s4np.add_question_to_test(user_id, project_name)
            
 
 
