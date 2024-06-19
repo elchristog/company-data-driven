948e1e47ef81ff539b5d4239344bb9bb0f76f3dc
Christian <elchristog@gmail.com>
2024-06-19 13:50:52 -0400

    Update program_progress.py
        if user_id == 1:

--- modules/program_progress.py
+++ modules/program_progress.py
@@ -139,7 +139,7 @@ def customer_success_crm_add_contact(user_id, project_name):
     os.write(1, '🥏 Executing customer_success_crm_add_contact \n'.encode('utf-8'))
     os.write(1, '- customer_success_crm_add_contact: Showing form \n'.encode('utf-8'))
 
-    if user_id = 1:
+    if user_id == 1:
         rows = uc.run_query_instant(f"SELECT u.username, u.id, c.id as contract_id FROM `company-data-driven.{project_name}.contracts` AS c INNER JOIN `company-data-driven.global.users` AS u ON u.id = c.user_id ORDER BY u.username;")
     else:
         rows = uc.run_query_instant(f"SELECT u.username, u.id, c.id as contract_id FROM `company-data-driven.{project_name}.contracts` AS c INNER JOIN `company-data-driven.global.users` AS u ON u.id = c.user_id LEFT JOIN `company-data-driven.{project_name}.program_customer_mentor_assignation` AS pcma ON c.user_id = pcma.customer_id WHERE pcma.mentor_id = {user_id}  ORDER BY u.username;")
