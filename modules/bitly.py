9003844d750c35cb4bfedbaee5b7e8d0cab72f02
Christian <elchristog@gmail.com>
2024-08-07 05:39:44 -0400

    Update bitly.py
    @st.fragment

--- modules/bitly.py
+++ modules/bitly.py
@@ -12,6 +12,7 @@ from streamlit_raw_echarts import st_echarts, JsCode
 
 import utils.user_credentials as uc
 
+@st.fragment
 def get_clicks_for_bitlink(token, bitlink, unit, units):
   url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks"
   headers = {"Authorization": f"Bearer {token}"}
@@ -20,6 +21,7 @@ def get_clicks_for_bitlink(token, bitlink, unit, units):
   response.raise_for_status()
   return response.json()
 
+@st.fragment
 def save_bitly_metrics_one_link(project_name, bitly_link, link_name, max_stored_date, current_date):
   access_token = st.secrets["BITLY_TOKEN"]
   bitlink = bitly_link
@@ -48,7 +50,7 @@ def save_bitly_metrics_one_link(project_name, bitly_link, link_name, max_stored_
 
 
 
-
+@st.fragment
 def save_bitly_metrics_bulk(project_name):
   os.write(1, '🥏 Executing save_bitly_metrics_bulk \n'.encode('utf-8'))
   if 'save_bitly_metrics_bulk_days_last_update' not in st.session_state:
